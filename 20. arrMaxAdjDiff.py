735a0536148e8450a0b2d4bfe89662a4c316fe37
Lee Hwang Tran <lehoangtran289@gmail.com>
2020-02-21 23:54:21 +0700

    del

--- 20. arrMaxAdjDiff.py
+++
@@ -1,6 +0,0 @@
-def arrayMaximalAdjacentDifference(arr):
-    max = -9999999
-    for i in range(1, len(arr)):
-        if (abs(arr[i] - arr[i-1]) > max):
-            max = abs(arr[i] - arr[i-1])
-    return max
--- 27. depositProfit.py
+++
@@ -1,9 +0,0 @@
-def depositProfit(n, r, max, count):
-    if n >= max:
-        return count
-    else:
-        n = n + r*n/100
-        count += 1
-        return depositProfit(n, r, max, count)
-
-print(depositProfit(100, 20, 170, 0))
