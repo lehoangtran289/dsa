d3bd56aefac450b659fc37fd1821ff5438daff53
Lee Hwang Tran <lehoangtran289@gmail.com>
2020-02-28 09:00:20 +0700

    28.02.20

--- 57. fileNaming.py
+++ 57. fileNaming.py
@@ -1,5 +1,22 @@
 def fileNaming(arr):
-    
+    flag = [0] * len(arr)
+    for i in range(len(arr)):
+        if flag[i] == 0:
+            flag[i] = 1
+            k = 1
+            for j in range(i+1, len(arr)):
+                if arr[j] == arr[i]:
+                    temp = arr[j] + '(' + str(k) + ')'
+                    while True:
+                        if temp in arr[:j+1]:
+                            k += 1
+                            temp = arr[j] + '(' + str(k) + ')'
+                        else: 
+                            break
+                    arr[j] = temp
+                    k += 1
+    return arr
 
-names = ["doc", "doc", "image", "doc(1)", "doc"]
+
+names = ["a(1)","a(6)","a","a","a","a","a","a","a","a","a","a"]
 print(fileNaming(names))
