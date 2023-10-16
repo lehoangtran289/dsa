43c037cf951fc960fd9b582021dab43d68bd5b89
lehoangtran289 <lehoangtran289@gmail.com>
2023-10-17 00:54:07 +0700

    update

--- leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1091_ShortestPathInBinaryMatrix.java
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1091_ShortestPathInBinaryMatrix.java
@@ -2,38 +2,10 @@ package com.leetcode.graphtheory;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
 
 public class _1091_ShortestPathInBinaryMatrix {
 public static void main(String[] args) {
         System.out.println(shortestPathBinaryMatrix(new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}}));
-    }
-
-    public static int shortestPathBinaryMatrix2(int[][] grid) {
-        int n = grid.length;
-        grid[0][0] = 1;
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {0, 0});
-
-        for (int ans = 1; !q.isEmpty(); ++ans) {
-            for (int k = q.size(); k > 0; --k) {
-                var p = q.poll();
-                int i = p[0], j = p[1];
-                if (i == n - 1 && j == n - 1) {
-                    return ans;
-                }
-                for (int x = i - 1; x <= i + 1; ++x) {
-                    for (int y = j - 1; y <= j + 1; ++y) {
-                        if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0) {
-                            grid[x][y] = 1;
-                            q.offer(new int[] {x, y});
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
     }
 
     public static int shortestPathBinaryMatrix(int[][] grid) {
