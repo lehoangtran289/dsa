a5fd2266e55e40a04f491ea4136ed711be0922ab
lehoangtran289 <lehoangtran289@gmail.com>
2023-10-19 11:31:03 +0700

    update

--- leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1293_ShortestPathInGridwObstacles.java
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1293_ShortestPathInGridwObstacles.java
@@ -20,41 +20,45 @@ public class _1293_ShortestPathInGridwObstacles {
         int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int n = grid.length;
         int m = grid[0].length;
-
         Queue<int[]> q = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[n][m][k+1];
-        visited[0][0][0] = true;
-        q.offer(new int[]{0, 0, 0});
+        boolean[][][] visited = new boolean[n][m][k+1]; // k+1 since k is not index
         int res = 0;
 
+        visited[0][0][k] = true;
+        q.offer(new int[]{0, 0, k}); // row, col, K balance
         while (!q.isEmpty()) {
             int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] info = q.poll();
-                int r = info[0];
-                int c = info[1];
-                int curK = info[2];
 
-                if (r == n-1 && c == m-1) {
+            while (size > 0) {
+                int[] cur = q.poll();
+                int r = cur[0];
+                int c = cur[1];
+                int remainK = cur[2];
+
+                if (r == n - 1 && c == m - 1) // reach dest
                     return res;
-                }
 
-                for (int[] dir: dirs) {
-                    int nextR = dir[0] + r;
-                    int nextC = dir[1] + c;
-                    int nextK = curK;
-                    if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m) {
-                        if (grid[nextR][nextC] == 1) {
-                            nextK++;
+                for (int[] dir : dirs) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+
+                    if (grid[nr][nc] == 1) { // meet obstacles -> decrease remainK
+                        if (remainK > 0 && !visited[nr][nc][remainK - 1]) {
+                            visited[nr][nc][remainK - 1] = true;
+                            q.offer(new int[]{nr, nc, remainK - 1});
                         }
-                        if (nextK <= k && !visited[nextR][nextC][nextK]) {
-                            visited[nextR][nextC][nextK] = true;
-                            q.offer(new int[]{nextR, nextC, nextK});
+                    } else { // no obstacle
+                        if (!visited[nr][nc][remainK]) {
+                            visited[nr][nc][remainK] = true;
+                            q.offer(new int[]{nr, nc, remainK});
                         }
                     }
                 }
+                --size;
             }
-            res++;
+            ++res;
         }
         return -1;
     }
