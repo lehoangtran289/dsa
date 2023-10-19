61414de8d9eec29adae871bbcbfbddb97ffa70d7
lehoangtran289 <lehoangtran289@gmail.com>
2023-10-19 10:20:19 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1293_ShortestPathInGridwObstacles.java
@@ -0,0 +1,61 @@
+package com.leetcode.graphtheory;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class _1293_ShortestPathInGridwObstacles {
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {0, 0, 0},
+                {1, 1, 0},
+                {0, 0, 0},
+                {0, 1, 1},
+                {0, 0, 0}
+        };
+        System.out.println(shortestPath(grid, 1));
+    }
+
+    public static int shortestPath(int[][] grid, int k) {
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][][] visited = new boolean[n][m][k+1];
+        visited[0][0][0] = true;
+        q.offer(new int[]{0, 0, 0});
+        int res = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] info = q.poll();
+                int r = info[0];
+                int c = info[1];
+                int curK = info[2];
+
+                if (r == n-1 && c == m-1) {
+                    return res;
+                }
+
+                for (int[] dir: dirs) {
+                    int nextR = dir[0] + r;
+                    int nextC = dir[1] + c;
+                    int nextK = curK;
+                    if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m) {
+                        if (grid[nextR][nextC] == 1) {
+                            nextK++;
+                        }
+                        if (nextK <= k && !visited[nextR][nextC][nextK]) {
+                            visited[nextR][nextC][nextK] = true;
+                            q.offer(new int[]{nextR, nextC, nextK});
+                        }
+                    }
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+}
