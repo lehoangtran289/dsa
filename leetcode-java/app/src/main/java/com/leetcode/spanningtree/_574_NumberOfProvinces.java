48405b68fe4be0a9d47de945a67a4173d7b3fcfe
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-14 02:35:35 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/leetcode/spanningtree/_574_NumberOfProvinces.java
@@ -0,0 +1,29 @@
+package com.leetcode.spanningtree;
+
+public class _574_NumberOfProvinces {
+
+    public static void main(String[] args) {
+        System.out.println(findCircleNum(new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+    }
+
+    public static int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        _0_DisjointSets ds = new _0_DisjointSets(n);
+
+        for (int u = 0; u < n; u++) {
+            for (int v = u + 1; v < n; v++) {
+                if (isConnected[u][v] == 1) {
+                    ds.union(u, v);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.find(i) == i) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
