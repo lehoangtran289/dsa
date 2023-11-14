01696b5a12862d2cdb3323245d8e3808b2b93f8a
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-14 12:24:17 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/leetcode/treeheap/_215_KthLargestElementInAnArray.java
@@ -0,0 +1,27 @@
+package com.leetcode.treeheap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class _215_KthLargestElementInAnArray {
+    public static void main(String[] args) {
+        System.out.println(new _215_KthLargestElementInAnArray().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(new _215_KthLargestElementInAnArray().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        System.out.println(new _215_KthLargestElementInAnArray().findKthLargest(new int[]{1}, 1));
+    }
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i < k) {
+                pq.offer(nums[i]);
+                continue;
+            }
+            if (pq.peek() < nums[i]) {
+                pq.poll();
+                pq.offer(nums[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+}
