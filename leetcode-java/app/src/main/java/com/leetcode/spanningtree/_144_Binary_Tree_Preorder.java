a41d30cf1d33f2645e123c8693fd84eedda2d472
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-14 09:32:34 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/leetcode/spanningtree/_144_Binary_Tree_Preorder.java
@@ -0,0 +1,25 @@
+package com.leetcode.spanningtree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _144_Binary_Tree_Preorder {
+    public static void main(String[] args) {
+        System.out.println(preorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null))));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        visit(root, res);
+        return res;
+    }
+
+    private static void visit(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        visit(root.left, res);
+        visit(root.right, res);
+    }
+}
+
+
---
+++ leetcode-java/app/src/main/java/com/leetcode/spanningtree/_145_Binary_Tree_Postorder.java
@@ -0,0 +1,25 @@
+package com.leetcode.spanningtree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _145_Binary_Tree_Postorder {
+    public static void main(String[] args) {
+        System.out.println(preorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null))));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        visit(root, res);
+        return res;
+    }
+
+    private static void visit(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        visit(root.left, res);
+        visit(root.right, res);
+        res.add(root.val);
+    }
+}
+
+
