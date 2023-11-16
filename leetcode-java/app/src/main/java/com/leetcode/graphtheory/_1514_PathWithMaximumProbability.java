c1b24dd6c922281430659546344eb893f176b7d3
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-16 15:22:08 +0700

    update version

--- leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1514_PathWithMaximumProbability.java
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_1514_PathWithMaximumProbability.java
@@ -9,18 +9,18 @@ public class _1514_PathWithMaximumProbability {
                 , 0, 2));
     }
 
-    static class Node {
+    static class Edge {
         int v;
         double w;
 
-        public Node(int v, double w) {
+        public Edge(int v, double w) {
             this.v = v;
             this.w = w;
         }
     }
 
     public static double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        List<Node>[] graph = new List[n];
+        List<Edge>[] graph = new List[n];
         for (int i = 0; i < n; ++i) {
             graph[i] = new ArrayList<>();
         }
@@ -30,39 +30,35 @@ public class _1514_PathWithMaximumProbability {
             int u = e[0];
             int v = e[1];
             double w = succProb[i];
-            graph[u].add(new Node(v, w));
-            graph[v].add(new Node(u, w));
+            graph[u].add(new Edge(v, w));
+            graph[v].add(new Edge(u, w));
         }
 
         return dijkstra(graph, start_node, end_node);
     }
 
-    public static double dijkstra(List<Node>[] graph, int src, int dst) {
+    public static double dijkstra(List<Edge>[] graph, int src, int dst) {
         int n = graph.length;
-        boolean[] seen = new boolean[n];
+        double[] dist = new double[n];
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.w, o1.w));
-        pq.offer(new Node(src, 1.0));
+        dist[src] = 1.0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.w, o1.w));
+        pq.offer(new Edge(src, 1.0));
 
         while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int u = node.v;
-            if (u == dst) {
-                return node.w;
-            }
-            if (seen[u]) {
-                continue;
-            }
-            seen[u] = true;
-            for (Node edge : graph[u]) {
+            Edge e = pq.poll();
+            int u = e.v;
+
+            for (Edge edge : graph[u]) {
                 int v = edge.v;
                 double vW = edge.w;
-                if (seen[v]) {
-                    continue;
+                double tempDist = dist[u] * vW;
+                if (tempDist > dist[v]) {
+                    dist[v] = tempDist;
+                    pq.offer(new Edge(v, tempDist));
                 }
-                pq.offer(new Node(v, node.w * vW));
             }
         }
-        return 0;
+        return dist[dst] != 0 ? dist[dst] : 0;
     }
 }
