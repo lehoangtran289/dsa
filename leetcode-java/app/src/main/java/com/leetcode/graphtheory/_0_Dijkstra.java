cf0bfc5014a3f7ab1212fb0d22a3aca36d0c92ce
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-16 12:31:39 +0700

    update version

--- leetcode-java/app/src/main/java/com/leetcode/graphtheory/_0_Dijkstra.java
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_0_Dijkstra.java
@@ -3,7 +3,7 @@ package com.leetcode.graphtheory;
 import java.util.*;
 
 public class _0_Dijkstra {
-    public static void dijkstra(int[][] graph, int s) {
+    public static void dijkstra(List<int[]>[] graph, int s) {
         int n = graph.length;
         Set<Integer> visited = new HashSet<>();
         int[] dist = new int[n];
@@ -12,21 +12,23 @@ public class _0_Dijkstra {
         Arrays.fill(dist, Integer.MAX_VALUE);
         Arrays.fill(prev, -1);
         dist[s] = 0;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(u -> dist[u]));
-        pq.offer(s);
+        PriorityQueue<Integer[]> pq = new PriorityQueue<>(Comparator.comparingInt(u -> dist[u[0]]));
+        pq.offer(new Integer[]{s, 0});
 
         while (!pq.isEmpty()) {
-            int u = pq.poll();
+            Integer[] p = pq.poll();
+            int u = p[0];
             visited.add(u);
 
-            for (int v = 0; v < n; v++) {
-                if (graph[u][v] != 0 && !visited.contains(v)) { // if edge (u, v) exists
-                    int tempDist = dist[u] + graph[u][v];
+            for (int[] edge : graph[u]) {
+                int v = edge[0];
+                int vW = edge[1];
+                if (!visited.contains(v)) { // if edge (u, v) exists
+                    int tempDist = dist[u] + vW;
                     if (tempDist < dist[v]) {
                         dist[v] = tempDist;
                         prev[v] = u;
-                        pq.offer(v);
+                        pq.offer(new Integer[]{v, tempDist});
                     }
                 }
             }
@@ -52,14 +54,28 @@ public class _0_Dijkstra {
     }
 
     public static void main(String[] args) {
-        int[][] graph = new int[][]{
-                {0, 1, 4, 0, 0, 0},
-                {1, 0, 2, 3, 6, 1},
-                {4, 2, 0, 0, 0, 0},
-                {0, 3, 0, 0, 2, 0},
-                {0, 6, 0, 2, 0, 3},
-                {0, 1, 0, 0, 3, 0}
-        };
+        List<int[]>[] graph = new List[6];
+        for (int i = 0; i < 6; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new int[]{1, 1});
+        graph[0].add(new int[]{2, 4});
+        graph[1].add(new int[]{0, 1});
+        graph[1].add(new int[]{2, 2});
+        graph[1].add(new int[]{3, 3});
+        graph[1].add(new int[]{4, 6});
+        graph[1].add(new int[]{5, 1});
+        graph[2].add(new int[]{1, 2});
+        graph[2].add(new int[]{0, 4});
+        graph[3].add(new int[]{1, 3});
+        graph[3].add(new int[]{4, 2});
+        graph[4].add(new int[]{1, 6});
+        graph[4].add(new int[]{3, 2});
+        graph[4].add(new int[]{5, 3});
+        graph[5].add(new int[]{1, 1});
+        graph[5].add(new int[]{4, 3});
+
         dijkstra(graph, 0);
     }
 }
