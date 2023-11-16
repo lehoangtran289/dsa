93b4078be0d37cc7c5280422385854e9d79e71d8
lehoangtran289 <lehoangtran289@gmail.com>
2023-11-16 11:18:18 +0700

    update version

--- leetcode-java/app/src/main/java/com/leetcode/graphtheory/_0_Dijkstra.java
+++ leetcode-java/app/src/main/java/com/leetcode/graphtheory/_0_Dijkstra.java
@@ -1,33 +1,30 @@
 package com.leetcode.graphtheory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class _0_Dijkstra {
     public static void dijkstra(int[][] graph, int s) {
         int n = graph.length;
-        boolean[] visitedVertex = new boolean[n];
-        int[] distance = new int[n];
+        Set<Integer> visited = new HashSet<>();
+        int[] dist = new int[n];
         int[] prev = new int[n];
 
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(dist, Integer.MAX_VALUE);
         Arrays.fill(prev, -1);
-        distance[s] = 0;
+        dist[s] = 0;
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.offer(s);
 
         while (!pq.isEmpty()) {
             int u = pq.poll();
-            visitedVertex[u] = true;
+            visited.add(u);
 
             for (int v = 0; v < n; v++) {
-                if (graph[u][v] != 0 && !visitedVertex[v]) { // if edge (u, v) exists
-                    int newDist = distance[u] + graph[u][v];
-                    if (newDist < distance[v]) {
-                        distance[v] = newDist;
+                if (graph[u][v] != 0 && !visited.contains(v)) { // if edge (u, v) exists
+                    int tempDist = dist[u] + graph[u][v];
+                    if (tempDist < dist[v]) {
+                        dist[v] = tempDist;
                         prev[v] = u;
                         pq.offer(v);
                     }
@@ -39,7 +36,7 @@ public class _0_Dijkstra {
         System.out.println("Shortest distances from node " + s + ":");
         for (int i = 0; i < n; i++) {
             if (i == s) continue;
-            System.out.println("\nTo node " + i + ": " + distance[i]);
+            System.out.println("\nTo node " + i + ": " + dist[i]);
             ArrayList<Integer> path = new ArrayList<>();
             int cur = i;
             while (cur != -1) {
@@ -48,14 +45,14 @@ public class _0_Dijkstra {
             }
             Collections.reverse(path);
             System.out.println("Path: " + path);
-            System.out.println("Distance: " + distance[i]);
+            System.out.println("Distance: " + dist[i]);
         }
     }
 
     public static void main(String[] args) {
         int[][] graph = new int[][]{
-                {0, 4, 4, 0, 0, 0},
-                {4, 0, 2, 3, 6, 1},
+                {0, 1, 4, 0, 0, 0},
+                {1, 0, 2, 3, 6, 1},
                 {4, 2, 0, 0, 0, 0},
                 {0, 3, 0, 0, 2, 0},
                 {0, 6, 0, 2, 0, 3},
