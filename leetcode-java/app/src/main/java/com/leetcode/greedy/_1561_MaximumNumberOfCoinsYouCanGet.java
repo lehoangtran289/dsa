219b71a2a510e07444143128805eafc8f609e2ed
lehoangtran289 <lehoangtran289@gmail.com>
2023-12-05 14:33:15 +0700

    update

--- leetcode-java/app/src/main/java/com/leetcode/greedy/_1561_MaximumNumberOfCoinsYouCanGet.java
+++ leetcode-java/app/src/main/java/com/leetcode/greedy/_1561_MaximumNumberOfCoinsYouCanGet.java
@@ -1,4 +1,19 @@
 package com.leetcode.greedy;
 
+import java.util.Arrays;
+
 public class _1561_MaximumNumberOfCoinsYouCanGet {
+    public static void main(String[] args) {
+        System.out.println(maxCoins(new int[]{9,8,7,6,5,4,3,2,1}));
+    }
+
+    public static int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int res = 0;
+
+        for (int i = piles.length / 3; i < piles.length; i += 2)
+            res += piles[i];
+
+        return res;
+    }
 }
