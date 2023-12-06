e00664be1a349a15d38dbdbb3843822316cc8ce5
lehoangtran289 <lehoangtran289@gmail.com>
2023-12-06 16:42:22 +0700

    update

--- leetcode-java/app/src/main/java/com/leetcode/greedy/_1402_ReducingDishes.java
+++ leetcode-java/app/src/main/java/com/leetcode/greedy/_1402_ReducingDishes.java
@@ -1,11 +1,24 @@
 package com.leetcode.greedy;
 
+import java.util.Arrays;
+
 public class _1402_ReducingDishes {
     public static void main(String[] args) {
         System.out.println(maxSatisfaction(new int[]{-1, -8, 0, 5, -9}));
     }
 
     public static int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int res = 0;
+        int curSum = 0;
+        for (int i = satisfaction.length - 1; i >= 0; --i) {
+            curSum += satisfaction[i];
 
+            // res[i] = res[i-1] + sum(0 -> i)
+            // curSum < 0 means res[i] is decreasing -> stop
+            if (curSum < 0) return res;
+            else res += curSum;
+        }
+        return res;
     }
 }
