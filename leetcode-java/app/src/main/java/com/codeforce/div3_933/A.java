5c9c4b62af058b479e6eef3b02b42ab09f1eba8a
lehoangtran289 <lehoangtran289@gmail.com>
2024-03-12 00:39:53 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/codeforce/div3_933/A.java
@@ -0,0 +1,90 @@
+package com.codeforce.div3_933;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+    static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+
+        // Start writing your solution here. -------------------------------------
+        int tests = sc.nextInt(); // number of test cases
+        while (tests-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int k = sc.nextInt();
+            Integer[] left = new Integer[n];
+            for (int i = 0; i < n; i++)
+                left[i] = sc.nextInt();
+            Integer[] right = new Integer[m];
+            for (int i = 0; i < m; i++)
+                right[i] = sc.nextInt();
+            solution(n, m, left, right, k);
+        }
+
+        // Stop writing your solution here. -------------------------------------
+    }
+
+    private static void solution(int n, int m, Integer[] l, Integer[] r, int k) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (l[i] + r[j] <= k) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
