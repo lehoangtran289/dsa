36563c02f1c5447c765a4e64a11cf396bf366908
Tran Le Hoang <lehoangtran289@gmail.com>
2024-04-08 23:51:54 +0700

    Create _1835_XORSumOfAND.java

---
+++ leetcode-java/app/src/main/java/com/leetcode/bit/_1835_XORSumOfAND.java
@@ -0,0 +1,33 @@
+package com.leetcode.bit;
+
+import java.util.*;
+
+public class _1835_XORSumOfAND {
+    public int getXORSum(int[] arr1, int[] arr2) {
+        int[] bitCount2 = new int[30];
+        for (int x : arr2) {
+            for (int i = bitCount2.length - 1; i >= 0; --i) {
+                if (((x >> i) & 1) == 1) {
+                    bitCount2[i]++;
+                }
+            }
+        }
+
+        int[] xorResult = new int[30];
+        for (int x : arr1) {
+            for (int i = xorResult.length - 1; i >= 0; --i) {
+                if (((x >> i) & 1) == 1) {
+                    xorResult[i] += bitCount2[i];
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = xorResult.length - 1; i >= 0; --i) {
+            if (xorResult[i] % 2 != 0) {
+                result += 1 << i;
+            }
+        }
+        return result;
+    }
+}
