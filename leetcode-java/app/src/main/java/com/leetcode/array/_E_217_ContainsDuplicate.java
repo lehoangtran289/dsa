dbfab9cbc0205de81304cffaf5e87f99e8dbb934
lehoangtran289 <lehoangtran289@gmail.com>
2024-04-10 22:34:21 +0700

    update

---
+++ leetcode-java/app/src/main/java/com/leetcode/array/_E_217_ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package com.leetcode.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _E_217_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            if (set.contains(x)) return true;
+            set.add(x);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        _E_217_ContainsDuplicate obj = new _E_217_ContainsDuplicate();
+        System.out.println(obj.containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+}
