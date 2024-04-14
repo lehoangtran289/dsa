8c7ecb5f66951429952cf74cd204682c4cb6dd0e
lehoangtran289 <lehoangtran289@gmail.com>
2024-04-15 00:45:13 +0700

    update

--- leetcode-java/app/src/main/java/com/leetcode/array/_M_15_ThreeSum.java
+++ leetcode-java/app/src/main/java/com/leetcode/array/_M_15_ThreeSum.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class _15_ThreeSum {
+public class _M_15_ThreeSum {
     public static void main(String[] args) {
         System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
         System.out.println(threeSum(new int[]{0, 1, 1}));
