958ef2fa62b1ebe36a4dae380bbb13fff1e92408
htl982 <lehoangtran289@gmail.com>
2024-08-23 06:46:24 +0700

    update

---
+++ leetcode/src/main/java/leetcode/VPC/VPC_F_NoiTu.java
@@ -0,0 +1,363 @@
+package leetcode.VPC;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class VPC_F_NoiTu {
+
+    private static final boolean IS_LOCAL = true;
+    private static final String INPUT_FILE = "src/main/java/leetcode/VPC/input/F.inp";
+    private final static FastReader reader;
+    private final static String YES = "YES";
+    private final static String NO = "NO";
+    private final static long mod = (long) 1e9 + 7;
+
+    // ----------------------------------------------------------------
+
+    static {
+        InputStream is;
+        try {
+            is = IS_LOCAL ? Files.newInputStream(new File(INPUT_FILE).toPath()) : System.in;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        reader = new FastReader(is);
+    }
+
+    public static void main(String[] args) {
+        try {
+            PrintStream out = System.out;
+
+            // INPUT -----------------------------------------------
+            int n = readInt();
+            List<String> dict = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                dict.add(readStr());
+            }
+
+            int q = readInt();
+            List<String> queries = new ArrayList<>();
+            for (int i = 0; i < q; ++i) {
+                queries.add(readStr());
+            }
+
+            // SOLUTION --------------------------------------------
+
+            Trie trie = new Trie();
+            for (String s : dict) {
+                trie.insert(s);
+            }
+
+            for (String s : queries) {
+                List<String> res = new ArrayList<>();
+                out.println(sol(s, trie, 0, s.length(), res));
+            }
+            
+            // ----------------------------------------------
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String sol(String s, Trie trie, int start, int end, List<String> res) {
+        if (start == end) {
+            return String.join(" ", res);
+        }
+
+        for (int i = start; i < end; ++i) {
+            if (trie.search(s.substring(start, i + 1))) {
+                res.add(s.substring(start, i + 1));
+                String ans = sol(s, trie, i + 1, end, res);
+                if (!ans.equals("-1")) {
+                    return ans;
+                }
+                res.remove(res.size() - 1);
+            }
+        }
+
+        return "-1";
+    }
+
+    static class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        boolean isLeaf;
+
+        TrieNode() {
+            Arrays.fill(child, null);
+            isLeaf = false;
+        }
+    }
+
+    static class Trie {
+        TrieNode root;
+
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String key) {
+            TrieNode node = root;
+            for (char chr : key.toCharArray()) {
+                int index = chr - 'a';
+                if (node.child[index] == null) {
+                    node.child[index] = new TrieNode(); // If node for current character does not exist then make a new node
+                }
+                node = node.child[index]; // Move the curr pointer to the newly created node
+            }
+            node.isLeaf = true;
+        }
+
+        boolean search(String key) {
+            TrieNode node = root;
+            for (char chr : key.toCharArray()) {
+                int index = chr - 'a';
+                if (node.child[index] == null) {
+                    return false;
+                }
+                node = node.child[index];
+            }
+            return node != null && node.isLeaf;
+        }
+
+        boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for (char chr : prefix.toCharArray()) {
+                int index = chr - 'a';
+                if (node.child[index] == null) {
+                    return false;
+                }
+                node = node.child[index];
+            }
+            return node != null;
+        }
+
+        void display(TrieNode node, char[] str, int level) {
+            if (node.isLeaf) {
+                str[level] = '\0';
+                System.out.println(new String(str, 0, level));
+            }
+            for (int i = 0; i < 26; i++) {
+                if (node.child[i] != null) {
+                    str[level] = (char) (i + 'a');
+                    display(node.child[i], str, level + 1);
+                }
+            }
+        }
+    }
+
+    // ======================================================================================
+
+    private static String[] stringArray(int n, boolean oneIndexed) {
+        int i = 0;
+        String s[] = new String[n];
+        if (oneIndexed) {
+            i = 1;
+            s = new String[n + 1];
+            n++;
+        }
+
+        for (; i < n; i++) {
+            s[i] = reader.next();
+        }
+        return s;
+    }
+
+    private static long readLong() {
+        return reader.nextLong();
+    }
+
+    private static int[] intArray(int n, boolean oneIndexed) {
+        int i = 0;
+        int arr[] = new int[n];
+        if (oneIndexed) {
+            i = 1;
+            arr = new int[n + 1];
+            n++;
+        }
+        for (; i < n; i++) {
+            arr[i] = reader.nextInt();
+        }
+        return arr;
+    }
+
+    private static long[] longArray(int n, boolean oneIndexed) {
+        long arr[] = new long[n];
+        int i = 0;
+        if (oneIndexed) {
+            i = 1;
+            arr = new long[n + 1];
+            n++;
+        }
+        for (; i < n; i++) {
+            arr[i] = reader.nextLong();
+        }
+        return arr;
+    }
+
+    private static char[] charArray() {
+        return readStr().toCharArray();
+    }
+
+    private static String readStr() {
+        return reader.next();
+    }
+
+    private static int readInt() {
+        return reader.nextInt();
+    }
+
+    private static void addToMap(int val, Map<Integer, Integer> map) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+    }
+
+    private static void removeFromMap(int val, Map<Integer, Integer> map) {
+        int count = map.get(val);
+        if (count == 1) map.remove(val);
+        else map.put(val, count - 1);
+    }
+
+    private static void addToMap(long val, Map<Long, Integer> map) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+    }
+
+    private static void removeFromMap(long val, Map<Long, Integer> map) {
+        int count = map.get(val);
+        if (count == 1) map.remove(val);
+        else map.put(val, count - 1);
+    }
+
+    private static void addToMap(char val, Map<Character, Integer> map) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+    }
+
+    private static void removeFromMap(char val, Map<Character, Integer> map) {
+        int count = map.get(val);
+        if (count == 1) map.remove(val);
+        else map.put(val, count - 1);
+    }
+
+    private static void addToMap(String val, Map<String, Integer> map) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+    }
+
+    private static void removeFromMap(String val, Map<String, Integer> map) {
+        int count = map.get(val);
+        if (count == 1) map.remove(val);
+        else map.put(val, count - 1);
+    }
+
+    private static int max(int... arr) {
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
+    private static int min(int... arr) {
+        return Arrays.stream(arr).min().getAsInt();
+    }
+
+    private static long min(long... arr) {
+        return Arrays.stream(arr).min().getAsLong();
+    }
+
+    private static long max(long... arr) {
+        return Arrays.stream(arr).max().getAsLong();
+    }
+
+    private static long gcd(long a, long b) {
+        if (a == 0) return b;
+
+        return gcd(b % a, a);
+    }
+
+    private static int gcd(int a, int b) {
+        if (a == 0) return b;
+
+        return gcd(b % a, a);
+    }
+
+    private long multiplyMod(long a, long b) {
+        return (a * b) % mod;
+    }
+
+    private long addMod(long a, long b) {
+        return (a + b) % mod;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] readIntArray(int arr[], int n) {
+            arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        long[] readLongArray(long arr[], int n) {
+            arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+
