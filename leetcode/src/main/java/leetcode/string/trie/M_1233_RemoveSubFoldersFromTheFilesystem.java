feef6009ab3ebbcad9bc04bb10e4476546820add
lehoangtran289 <lehoangtran289@gmail.com>
2024-10-26 00:00:48 +0700

    update gps

---
+++ leetcode/src/main/java/leetcode/string/trie/M_1233_RemoveSubFoldersFromTheFilesystem.java
@@ -0,0 +1,20 @@
+package leetcode.string.trie;
+
+import java.util.List;
+
+public class M_1233_RemoveSubFoldersFromTheFilesystem {
+    public static void main(String[] args) {
+        System.out.println(new M_1233_RemoveSubFoldersFromTheFilesystem().removeSubfolders(new String[]{"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"}));
+    }
+
+    public List<String> removeSubfolders(String[] folders) {
+        for (String folder : folders) {
+            String[] split = folder.split("/");
+
+
+
+        }
+
+        return null;
+    }
+}
