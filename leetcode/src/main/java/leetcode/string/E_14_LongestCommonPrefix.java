136ade3fc6a1f993a185d86a546bfeb966921831
htl982 <lehoangtran289@gmail.com>
2024-11-02 11:07:18 +0700

    E_14_LongestCommonPrefix

---
+++ leetcode/src/main/java/leetcode/string/E_14_LongestCommonPrefix.java
@@ -0,0 +1,37 @@
+package leetcode.string;
+
+public class E_14_LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"a","ab"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
+    }
+
+    /**
+     * Binary search on result
+     */
+    public static String longestCommonPrefix(String[] strs) {
+        int l = 0, r = Integer.MAX_VALUE;
+        for (String str : strs) {
+            r = Math.min(r, str.length());
+        }
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (isCommonPrefix(strs, mid)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return strs[0].substring(0, (l + r) / 2);
+    }
+
+    private static boolean isCommonPrefix(String[] strs, int len) {
+        String prefix = strs[0].substring(0, len);
+        for (String s : strs) {
+            if (!s.startsWith(prefix)) return false;
+        }
+        return true;
+    }
+}
