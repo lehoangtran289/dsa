325ff246334cad98b0ff731a6d2ad100ada250cb
lehoangtran289 <lehoangtran289@gmail.com>
2024-11-07 13:11:56 +0700

    M_2275_LargestCombinationWithBitwiseANDGreaterThanZero

--- leetcode/src/main/java/leetcode/bit/M_2275_LargestCombinationWithBitwiseANDGreaterThanZero.java
+++ leetcode/src/main/java/leetcode/bit/M_2275_LargestCombinationWithBitwiseANDGreaterThanZero.java
@@ -2,15 +2,20 @@ package leetcode.bit;
 
 public class M_2275_LargestCombinationWithBitwiseANDGreaterThanZero {
     public static void main(String[] args) {
-        System.out.println(largestCombination(new int[]{16, 17, 71, 62, 12, 24, 14}));
+        System.out.println(largestCombination(new int[]{16,16,48,71,62,12,24,14,17,18,19,20,10000}));
     }
 
     public static int largestCombination(int[] candidates) {
-        int[] bitIndex = new int[24];
+        int maxCandidate = 0;
+        for (int n : candidates) {
+            maxCandidate = Math.max(maxCandidate, n);
+        }
+
+        int[] bitIndex = new int[Integer.toBinaryString(maxCandidate).length()];
         int max = 0;
 
         for (int n : candidates) {
-            for (int i = 0; i < 24; ++i) {
+            for (int i = 0; i < bitIndex.length; ++i) {
                 if ((n & (1 << i)) != 0) {
                     bitIndex[i]++;
                     max = Math.max(bitIndex[i], max);
