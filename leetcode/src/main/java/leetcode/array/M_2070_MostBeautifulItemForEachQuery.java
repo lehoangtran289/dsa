92d1ef07bf755864e7b4d38683b0af6a8c77c838
lehoangtran289 <lehoangtran289@gmail.com>
2024-11-12 14:24:36 +0700

    M_2070_MostBeautifulItemForEachQuery

--- leetcode/src/main/java/leetcode/array/M_2070_MostBeautifulItemForEachQuery.java
+++ leetcode/src/main/java/leetcode/array/M_2070_MostBeautifulItemForEachQuery.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class M_2070_MostBeautifulItemForEachQuery {
     public static void main(String[] args) {
-        int[][] p = new int[][]{{193, 732}, {781, 962}, {864, 954}, {749, 627}, {136, 746}, {478, 548}, {640, 908}, {210, 799}, {567, 715}, {914, 388}, {487, 853}, {533, 554}, {247, 919}, {958, 150}, {193, 523}, {176, 656}, {395, 469}, {763, 821}, {542, 946}, {701, 676}};
-        int[] q = new int[]{885, 1445, 1580, 1309, 205, 1788, 1214, 1404, 572, 1170, 989, 265, 153, 151, 1479, 1180, 875, 276, 1584};
+        int[][] p = new int[][]{{10, 1000}};
+        int[] q = new int[]{5};
         System.out.println(Arrays.toString(maximumBeauty(p, q)));
     }
 
