4eb539e774cff2e7e42ab7085aa40e8e1a84d18f
htl982 <lehoangtran289@gmail.com>
2024-11-25 00:15:18 +0700

    M_1975_MaximumMatrixSum

---
+++ leetcode/src/main/java/leetcode/array/array2d/M_1975_MaximumMatrixSum.java
@@ -0,0 +1,32 @@
+package leetcode.array.array2d;
+
+public class M_1975_MaximumMatrixSum {
+    public static void main(String[] args) {
+        System.out.println(maxMatrixSum(new int[][]{
+                {2,9,3},
+                {5,4,-4},
+                {1,7,1}}
+        )); // 34
+        System.out.println(maxMatrixSum(new int[][]{{1,2,3},{-1,-2,-3},{1,2,3}})); // 16
+    }
+
+    public static long maxMatrixSum(int[][] matrix) {
+        long total = 0;
+        int minVal = Integer.MAX_VALUE;
+        int countNeg = 0;
+        boolean is0Exist = false;
+
+        for (int[] row : matrix) {
+            for (int i = 0; i < matrix[0].length; ++i) {
+                total += Math.abs(row[i]);
+                minVal = Math.min(minVal, Math.abs(row[i]));
+
+                if (row[i] == 0) is0Exist = true;
+                if (!is0Exist && row[i] < 0) countNeg++;
+            }
+        }
+
+        if (is0Exist) return total;
+        else return countNeg % 2 == 0 ? total : total - minVal * 2L;
+    }
+}
