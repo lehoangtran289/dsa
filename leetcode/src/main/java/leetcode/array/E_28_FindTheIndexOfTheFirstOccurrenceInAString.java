1e46178b967cf4f62174600e652f03dd9d9c5da1
htl982 <lehoangtran289@gmail.com>
2024-12-01 08:43:39 +0700

    E_28_FindTheIndexOfTheFirstOccurrenceInAString

---
+++ leetcode/src/main/java/leetcode/array/E_28_FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+public class E_28_FindTheIndexOfTheFirstOccurrenceInAString {
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi", "issi"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        for (int i = 0; i <= m - n; ++i) {
+            int p1 = i, p2 = 0;
+            while (p2 < n && haystack.charAt(p1) == needle.charAt(p2)) {
+                p1++;
+                p2++;
+            }
+            if (p2 == n) return i;
+        }
+
+        return -1;
+    }
+}
