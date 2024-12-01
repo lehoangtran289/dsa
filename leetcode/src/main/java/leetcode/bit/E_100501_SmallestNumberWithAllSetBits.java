26bb22a98f6b5f3644aea2a48a2af8de8947ef3c
htl982 <lehoangtran289@gmail.com>
2024-12-01 09:35:47 +0700

    E_100501_SmallestNumberWithAllSetBits

---
+++ leetcode/src/main/java/leetcode/bit/E_100501_SmallestNumberWithAllSetBits.java
@@ -0,0 +1,15 @@
+package leetcode.bit;
+
+public class E_100501_SmallestNumberWithAllSetBits {
+    public static void main(String[] args) {
+        System.out.println(smallestNumber(5));
+    }
+
+    public static int smallestNumber(int n) {
+        int i = 0;
+        while ((1 << i) - 1 < n) {
+            i++;
+        }
+        return (1 << i) - 1;
+    }
+}
