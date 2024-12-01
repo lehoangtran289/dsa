29906c8983750ae899e8ab61b1d67add59dbc6d2
htl982 <lehoangtran289@gmail.com>
2024-12-01 11:03:04 +0700

    Weekly426 - Q3

--- leetcode/src/main/java/leetcode/contest/weekly426/M_Q3_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI.java
+++ leetcode/src/main/java/leetcode/contest/weekly426/M_Q3_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 
-public class Q3_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI {
+public class M_Q3_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI {
     public static void main(String[] args) {
         int[][] edges1 = {{0, 1}, {0, 2}, {2, 3}, {2, 4}};
         int[][] edges2 = {{0, 1}, {0, 2}, {0, 3}, {2, 7}, {1, 4}, {4, 5}, {4, 6}};
