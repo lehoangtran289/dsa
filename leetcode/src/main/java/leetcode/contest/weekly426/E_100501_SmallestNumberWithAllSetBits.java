2f3998109cf24286596a5fc120dbebca95ba8ba7
lehoangtran289 <lehoangtran289@gmail.com>
2024-12-02 13:09:56 +0700

    E_100501_SmallestNumberWithAllSetBits

--- leetcode/src/main/java/leetcode/contest/weekly426/E_100501_SmallestNumberWithAllSetBits.java
+++ leetcode/src/main/java/leetcode/contest/weekly426/E_100501_SmallestNumberWithAllSetBits.java
@@ -6,6 +6,14 @@ public class E_100501_SmallestNumberWithAllSetBits {
     }
 
     public static int smallestNumber(int n) {
+        int i = 0;
+        while ((1 << i) - 1 < n) {
+            i++;
+        }
+        return (1 << i) - 1;
+    }
+
+    public static int smallestNumber2(int n) {
         int i = n + 1;
         while (true) {
             if (Integer.bitCount(i) == 1) return i - 1;
