eddea162b466226eb0ca9fa8e146d733838320a6
htl982 <lehoangtran289@gmail.com>
2024-12-09 00:43:41 +0700

    M_Q3_DigitOperationsToMakeTwoIntegersEqual

--- leetcode/src/main/java/contest/biweekly145/M_Q3_DigitOperationsToMakeTwoIntegersEqual.java
+++ leetcode/src/main/java/contest/biweekly145/M_Q3_DigitOperationsToMakeTwoIntegersEqual.java
@@ -1,115 +1,98 @@
 package contest.biweekly145;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import java.util.PriorityQueue;
 
 public class M_Q3_DigitOperationsToMakeTwoIntegersEqual {
     public static void main(String[] args) {
+        System.out.println(minOperations(15, 88));
         System.out.println(minOperations(10, 12));
     }
 
-    private static int transformDigits(String n, String m, int index, int cost) {
-        // Base case: If we've processed all digits, return the accumulated cost
-        if (index == n.length()) {
-            return cost;
-        }
+    public static int minOperations(int n, int m) {
+        if (m == n) return n;
 
-        // Get the current digit of n and m
-        int digitN = n.charAt(index) - '0';
-        int digitM = m.charAt(index) - '0';
+        boolean[] isPrime = buildPrimeFilter(10000);
+        if (isPrime[m] || isPrime[n]) return -1;
 
-        // Try to match the current digit of n to the corresponding digit of m
-        int currentNum = Integer.parseInt(n);
-        int newCost = cost + currentNum;
+        int[] cost = new int[10000];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[n] = n;
 
-        // If the current number is prime, we need to skip this digit transformation and move to the next
-        if (isPrime(currentNum)) {
-            return transformDigits(n, m, index + 1, newCost);
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); // pq by cost
+        pq.offer(new int[]{n, n});
 
-        // Try increasing the digit of n
-        if (digitN < digitM) {
-            StringBuilder newN = new StringBuilder(n);
-            newN.setCharAt(index, (char) (digitN + 1 + '0'));
-            int newNum = Integer.parseInt(newN.toString());
-            // Recursively transform the next digit, and add the cost for this operation
-            if (!isPrime(newNum)) {
-                return transformDigits(newN.toString(), m, index + 1, newCost + newNum);
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int curNum = cur[0];
+            int curCost = cur[1];
+
+            if (curNum == m) return curCost;
+
+            List<Integer> nextNums = getNextNums(curNum, isPrime);
+            for (int nextNum : nextNums) {
+                int newCost = curCost + nextNum;
+
+                if (newCost < cost[nextNum]) {
+                    cost[nextNum] = newCost;
+                    pq.offer(new int[]{nextNum, newCost});
+                }
             }
         }
 
-        // Try decreasing the digit of n
-        if (digitN > digitM) {
-            StringBuilder newN = new StringBuilder(n);
-            newN.setCharAt(index, (char) (digitN - 1 + '0'));
-            int newNum = Integer.parseInt(newN.toString());
-            // Recursively transform the next digit, and add the cost for this operation
-            if (!isPrime(newNum)) {
-                return transformDigits(newN.toString(), m, index + 1, newCost + newNum);
-            }
-        }
-
-        // Return -1 if we cannot transform the number to match m
         return -1;
     }
 
-    // Main function to call the recursive function and check the transformation cost
-    public static int minCost(int n, int m) {
-        // Convert n and m to strings
-        String nStr = String.valueOf(n);
-        String mStr = String.valueOf(m);
+    private static List<Integer> getNextNums(int curNum, boolean[] isPrime) {
+        List<Integer> res = new ArrayList<>();
 
-        // If n is prime initially, return -1 immediately
-        if (isPrime(n)) return -1;
+        char[] digits = String.valueOf(curNum).toCharArray();
+        for (int i = 0; i < digits.length; ++i) {
+            if (digits[i] < '9') {
+                char[] cloneArr = digits.clone();
+                cloneArr[i]++;
+                int newNum = Integer.parseInt(new String(cloneArr));
+                if (!isPrime[newNum]) {
+                    res.add(newNum);
+                }
+            }
 
-        // Call the recursive function to transform the digits and calculate the cost
-        return transformDigits(nStr, mStr, 0, 0);
-    }
-
-    public static int minOperations(int n, int m) {
-        return minCost(n, m);
-    }
-
-    private static List<Integer>[] createGraph(int n) {
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+            if (digits[i] > '0') {
+                char[] cloneArr = digits.clone();
+                cloneArr[i]--;
+                int newNum = Integer.parseInt(new String(cloneArr));
+                if (!isPrime[newNum]) {
+                    res.add(newNum);
+                }
+            }
         }
-        return graph;
+
+        return res;
     }
 
-    private static void addEdgeDi(List<Integer>[] graph, int from, int to) {
-        graph[from].add(to);
-    }
+    private static boolean[] buildPrimeFilter(int n) {
+        if (n <= 1) return new boolean[0];
 
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false;
+        boolean[] isPrimes = new boolean[n + 1];
+        Arrays.fill(isPrimes, true);
+        isPrimes[0] = false;
+        isPrimes[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(n); ++i) {
+            if (isPrimes[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrimes[j] = false;
+                }
+            }
         }
-        return true;
+
+        return isPrimes;
     }
 
-    public static List<Integer> numberToDigits(int num) {
-        List<Integer> digits = new ArrayList<>();
-        while (num > 0) {
-            digits.add(num % 10);
-            num /= 10;
-        }
-        Collections.reverse(digits);
-        return digits;
-    }
-
-    public static int digitsToNumber(List<Integer> digits) {
-        int num = 0;
-        for (int digit : digits) {
-            num = num * 10 + digit;
-        }
-        return num;
+    private static int getNumDigits(int num) {
+        if (num == 0) return 1;
+        return (int) Math.floor(Math.log10(num) + 1);
     }
 }
