4c1de677b9c844f0b6963e2719b06c945e81f678
lehoangtran289 <lehoangtran289@gmail.com>
2024-12-09 18:44:13 +0700

    M_973_KClosestPointsToOrigin

---
+++ leetcode/src/main/java/leetcode/array/M_973_KClosestPointsToOrigin.java
@@ -0,0 +1,28 @@
+package leetcode.array;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class M_973_KClosestPointsToOrigin {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(kClosest(new int[][]{{1, 3}, {-2, 2}}, 1)));
+    }
+
+    public static int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(a[0], b[0]));
+        for (int i = 0; i < points.length; ++i) {
+            pq.add(new double[]{distance(points[i][0], points[i][1]), i});
+        }
+
+        int[][] res = new int[k][];
+        for (int i = 0; i < k; ++i) {
+            double[] poll = pq.poll();
+            res[i] = points[(int) poll[1]];
+        }
+        return res;
+    }
+
+    private static double distance(int x, int y) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+}
