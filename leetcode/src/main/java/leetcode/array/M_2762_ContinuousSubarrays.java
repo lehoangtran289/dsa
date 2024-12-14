09658b77f2f619c3f757720c72a2227cf799d10d
htl982 <lehoangtran289@gmail.com>
2024-12-14 13:14:59 +0700

    M_2762_ContinuousSubarrays

--- leetcode/src/main/java/leetcode/array/M_2762_ContinuousSubarrays.java
+++ leetcode/src/main/java/leetcode/array/M_2762_ContinuousSubarrays.java
@@ -6,57 +6,59 @@ public class M_2762_ContinuousSubarrays {
         System.out.println(continuousSubarrays(new int[]{5, 4, 2, 4})); // 8
     }
 
+    /**
+     * Sliding window technique
+     * <pre>
+     * {@code
+     *      for (int l = 0, r = 0; r < n; r++) {
+     *         do_something_add(nums[r])
+     *         while (l < r and !condition) {
+     *             do_something_remove(nums[l]);
+     *             l++;
+     *         }
+     *         update_answer(r, l);
+     *      }
+     * }
+     * </pre>
+     */
     public static long continuousSubarrays(int[] nums) {
         long res = 0;
         int n = nums.length;
 
-        int l = 0, r = 0, windowSize = 0;
+        int l = 0, r, windowSize;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        while (r < n) {
+        for (r = 0; r < n; ++r) {
             max = Math.max(max, nums[r]);
             min = Math.min(min, nums[r]);
 
             if (max - min > 2) {
-                windowSize = r - l; // not + 1 because ++r
+                windowSize = r - l; // no need + 1 because r is not included
                 res += ((long) windowSize * (windowSize + 1)) / 2;
 
-                // start new window at r
+                // start new window at r -> max, min = nums[r]
                 l = r;
                 max = min = nums[r];
 
-                // expand window to the left
-                while (l > 0) {
-                    if (Math.abs(nums[r] - nums[l - 1]) <= 2) break;
-
+                // expand window to the left, update max, min while expanding
+                while (l > 0 && Math.abs(nums[r] - nums[l - 1]) <= 2) {
                     l--;
                     min = Math.min(min, nums[l]);
                     max = Math.max(max, nums[l]);
                 }
-                windowSize = r - l; // not + 1 because r is not included
+                windowSize = r - l; // no need + 1 because r is not included
                 res -= ((long) windowSize * (windowSize + 1)) / 2;
             }
-            ++r;
         }
 
-        windowSize = r - l; // not + 1 because ++r
+        windowSize = r - l; // not + 1 because r is not included
         res += ((long) windowSize * (windowSize + 1)) / 2;
 
         return res;
     }
 
-    /**
-     * Sliding window technique
-     * for(int l=0, r=0; r<n; r++){
-     * do_something_add(nums[r])
-     * while(l<r and !condition(k:=2)) {
-     * do_something_remove(nums[l]);
-     * l++;
-     * }
-     * update_answer(r, l);
-     * }
-     */
+
     public long continuousSubarraysBruteForce(int[] nums) {
         long res = 0;
         int n = nums.length;
