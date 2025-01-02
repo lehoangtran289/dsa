f84b36c9f1c4afbd08e4a55bcbc39f020f55c8c6
lehoangtran289 <lehoangtran289@gmail.com>
2025-01-02 09:07:45 +0700

    E_1422_MaximumScoreAfterSplittingAString

--- leetcode/src/main/java/leetcode/array/E_1422_MaximumScoreAfterSplittingAString.java
+++ leetcode/src/main/java/leetcode/array/E_1422_MaximumScoreAfterSplittingAString.java
@@ -5,6 +5,29 @@ public class E_1422_MaximumScoreAfterSplittingAString {
         System.out.println(maxScore("011101"));
     }
 
+    public static int maxScoreIntuitive(String s) {
+        int len = s.length();
+        int[] prefixZeros = new int[len];
+        int[] postfixOnes = new int[len];
+
+        prefixZeros[0] = s.charAt(0) == '0' ? 1 : 0;
+        postfixOnes[len - 1] = s.charAt(len - 1) == '1' ? 1 : 0;
+
+        for (int i = 1; i < len; ++i) {
+            prefixZeros[i] = prefixZeros[i - 1] + (s.charAt(i) == '0' ? 1 : 0);
+        }
+
+        for (int i = len - 2; i >= 0; --i) {
+            postfixOnes[i] = postfixOnes[i + 1] + (s.charAt(i) == '1' ? 1 : 0);
+        }
+
+        int res = 0;
+        for (int i = 0; i < len - 1; ++i) {
+            res = Math.max(res, prefixZeros[i] + postfixOnes[i + 1]);
+        }
+        return res;
+    }
+
     public static int maxScore(String s) {
         int len = s.length();
 
