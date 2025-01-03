4ddbded635176b6ebeacbd563b6823692c55e612
lehoangtran289 <lehoangtran289@gmail.com>
2025-01-03 15:06:03 +0700

    M_2270_NumberOfWaysToSplitArray

--- leetcode/src/main/java/leetcode/array/M_2270_NumberOfWaysToSplitArray.java
+++ leetcode/src/main/java/leetcode/array/M_2270_NumberOfWaysToSplitArray.java
@@ -1,8 +1,9 @@
 package leetcode.array;
 
+import java.util.Arrays;
+
 public class M_2270_NumberOfWaysToSplitArray {
     public static void main(String[] args) {
-        System.out.println(waysToSplitArray(new int[]{0, 0}));
         System.out.println(waysToSplitArray(new int[]{2, 3, 1, 0}));
     }
 
@@ -10,15 +11,15 @@ public class M_2270_NumberOfWaysToSplitArray {
         int n = nums.length;
 
         long[] prefixSum = new long[n + 1];
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < n; ++i) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        prefixSum[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
 
         int res = 0;
-        for (int i = 0; i < n - 1; ++i) {
+        for (int i = 1; i <= n - 1; ++i) {
             long left = prefixSum[i];
-            long right = prefixSum[n - 1] - prefixSum[i];
+            long right = prefixSum[n] - prefixSum[i];
             if (left >= right) res++;
         }
 
