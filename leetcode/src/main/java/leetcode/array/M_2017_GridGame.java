6afab25dacc5aa5320df26df39edf6fd18575898
lehoangtran289 <lehoangtran289@gmail.com>
2025-01-21 13:26:32 +0700

    M_2017_GridGame

---
+++ leetcode/src/main/java/leetcode/array/M_2017_GridGame.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+public class M_2017_GridGame {
+    public static void main(String[] args) {
+        System.out.println(gridGame(new int[][]{{20,3,20,17,2,12,15,17,4,15}, {20,10,13,14,15,5,2,3,14,3}}));
+    }
+
+    public static long gridGame(int[][] grid) {
+        long firstRowSum = 0;
+        for (int i = 0; i < grid[0].length; ++i) {
+            firstRowSum += grid[0][i];
+        }
+
+        long res = Long.MAX_VALUE;
+        long secondRowSum = 0;
+
+        for (int i = 0; i < grid[0].length; ++i) {
+            firstRowSum -= grid[0][i];
+
+            res = Math.min(res, Math.max(firstRowSum, secondRowSum));
+            secondRowSum += grid[1][i];
+        }
+
+        return res;
+    }
+}
