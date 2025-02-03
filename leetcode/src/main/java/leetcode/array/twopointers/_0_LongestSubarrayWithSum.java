1cdf1eb5386db25c69aba3c0cf52b92f0a01c003
lehoangtran289 <lehoangtran289@gmail.com>
2025-02-03 10:41:16 +0700

    Sliding window technique

---
+++ leetcode/src/main/java/leetcode/array/twopointers/_0_LongestSubarrayWithSum.java
@@ -0,0 +1,29 @@
+package leetcode.array.twopointers;
+
+public class _0_LongestSubarrayWithSum {
+    public static void main(String[] args) {
+        System.out.println(longestSubarrayWithSum(new int[]{1, 2, 1, 1, 1}, 3)); // 3
+    }
+
+    // Sliding window technique
+    public static int longestSubarrayWithSum(int[] nums, int k) {
+        int n = nums.length;
+
+        int res = 0;
+        int curSum = 0;
+        int l = 0;
+
+        for (int r = 0; r < n; ++r) {
+            curSum += nums[r];
+
+            while (curSum > k) {
+                curSum -= nums[l];
+                ++l;
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
