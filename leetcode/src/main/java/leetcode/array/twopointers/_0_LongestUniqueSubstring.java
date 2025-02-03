b602c9d1855fae31120a4fb46128d70e752ddd83
lehoangtran289 <lehoangtran289@gmail.com>
2025-02-03 10:44:33 +0700

    _0_LongestUniqueSubstring

---
+++ leetcode/src/main/java/leetcode/array/twopointers/_0_LongestUniqueSubstring.java
@@ -0,0 +1,31 @@
+package leetcode.array.twopointers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _0_LongestUniqueSubstring {
+    public static void main(String[] args) {
+        System.out.println(longestSubarrayWithSum("abaabbbbc")); // 3
+    }
+
+    // Sliding window technique
+    public static int longestSubarrayWithSum(String s) {
+        int res = 0;
+
+        int l = 0;
+        Set<Character> set = new HashSet<>();
+        for (int r = 0; r < s.length(); ++r) {
+            char curChar = s.charAt(r);
+
+            while (set.contains(curChar)) {
+                set.remove(s.charAt(l));
+                l++;
+            }
+
+            set.add(curChar);
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
