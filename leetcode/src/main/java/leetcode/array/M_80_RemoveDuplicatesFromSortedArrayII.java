5eb6a17dc0aa2db174476b8b634af00bda17b7c2
htl982 <lehoangtran289@gmail.com>
2025-02-03 23:47:46 +0700

    M_80_RemoveDuplicatesFromSortedArrayII generic

--- leetcode/src/main/java/leetcode/array/M_80_RemoveDuplicatesFromSortedArrayII.java
+++ leetcode/src/main/java/leetcode/array/M_80_RemoveDuplicatesFromSortedArrayII.java
@@ -2,8 +2,10 @@ package leetcode.array;
 
 public class M_80_RemoveDuplicatesFromSortedArrayII {
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+        System.out.println(removeDuplicates(new int[]{1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 1}));
     }
+
+    static final int MAX_DUPLICATES = 2;
 
     // two pointer to update the array in place
     public static int removeDuplicates(int[] nums) {
@@ -13,10 +15,12 @@ public class M_80_RemoveDuplicatesFromSortedArrayII {
         int pos = 0;
         for (int i = 0; i < n - 1; ++i) {
             // handle first element
+            int count = 1;
             nums[pos++] = nums[i];
 
             // handle next duplicate element
-            if (nums[i] == nums[i + 1]) {
+            while (nums[i] == nums[i + 1] && count < MAX_DUPLICATES) {
+                count++;
                 nums[pos++] = nums[i++];
             }
 
