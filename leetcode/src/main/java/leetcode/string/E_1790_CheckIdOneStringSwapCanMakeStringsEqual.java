6e4ea34736a6a6583f4ca9d2f33de87174cc87e3
lehoangtran289 <lehoangtran289@gmail.com>
2025-02-05 08:49:38 +0700

    E_1790_CheckIdOneStringSwapCanMakeStringsEqual

---
+++ leetcode/src/main/java/leetcode/string/E_1790_CheckIdOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,37 @@
+package leetcode.string;
+
+import java.util.ArrayDeque;
+
+public class E_1790_CheckIdOneStringSwapCanMakeStringsEqual {
+    public static void main(String[] args) {
+        System.out.println(areAlmostEqual("qgqeg", "gqgeq"));  // q g q g --- g q g q
+        System.out.println(areAlmostEqual("bank", "kanb")); // b k --- k b
+    }
+
+    public static boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+
+        ArrayDeque<Character> q1 = new ArrayDeque<>();
+        ArrayDeque<Character> q2 = new ArrayDeque<>();
+        int diffCount = 0;
+
+        for (int i = 0; i < n; ++i) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+
+            if (c1 != c2) {
+                q1.add(c1);
+                q2.add(c2);
+                diffCount++;
+            }
+        }
+
+        if (diffCount != 0 && diffCount != 2) return false;
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            if (q1.pollFirst() != q2.pollLast()) return false;
+        }
+
+        return true;
+    }
+}
