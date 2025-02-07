9c8927419c7a7a96a0a98f13b7c0ed8bb8ac62ad
lehoangtran289 <lehoangtran289@gmail.com>
2025-02-07 12:38:05 +0700

    M_3160_FindTheNumberOfDistinctColorsAmongTheBalls

---
+++ leetcode/src/main/java/leetcode/array/M_3160_FindTheNumberOfDistinctColorsAmongTheBalls.java
@@ -0,0 +1,38 @@
+package leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M_3160_FindTheNumberOfDistinctColorsAmongTheBalls {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] queryResults(int limit, int[][] queries) {
+        Map<Integer, Integer> ballMap = new HashMap<>();
+        Map<Integer, Integer> colorMap = new HashMap<>();
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; ++i) {
+            int ballIdx = queries[i][0];
+            int newColor = queries[i][1];
+
+            // remove old color
+            if (ballMap.containsKey(ballIdx)) {
+                int prevColor = ballMap.get(ballIdx);
+
+                colorMap.put(prevColor, colorMap.get(prevColor) - 1);
+                if (colorMap.get(prevColor) == 0) colorMap.remove(prevColor);
+            }
+
+            // set new color
+            ballMap.put(ballIdx, newColor);
+            colorMap.put(newColor, colorMap.getOrDefault(newColor, 0) + 1);
+
+            // set result
+            res[i] = colorMap.size();
+        }
+
+        return res;
+    }
+}
