673655f0a44bd9cce4e212b603d9f79b7ef9a931
htl982 <lehoangtran289@gmail.com>
2025-02-17 23:43:55 +0700

    H_489_RobotRoomCleaner

--- leetcode/src/main/java/leetcode/backtrack/H_489_RobotRoomCleaner.java
+++ leetcode/src/main/java/leetcode/backtrack/H_489_RobotRoomCleaner.java
@@ -3,6 +3,14 @@ package leetcode.backtrack;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 1. What to do if, after the right turn, there is an obstacle just in front? </br>
+ * >> Turn right again. </br>
+ * 2. How to explore the alternative paths from the cell?</br>
+ * >> Go back to that cell and then turn right from your last explored direction.</br>
+ * 3. When to stop? </br>
+ * >> Stop when you explored all possible paths, i.e. all 4 directions (up, right, down, and left) for each visited cell.</br>
+ */
 public class H_489_RobotRoomCleaner {
     // going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
     private static final int[][] DIRS = new int[][] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
@@ -12,6 +20,8 @@ public class H_489_RobotRoomCleaner {
         backtrack(robot, visited, 0, 0, 0);
     }
 
+    // we keep curDir to know the direction we are facing
+    // the order is important since the idea is always to turn right
     private static void backtrack(Robot robot, Set<Cell> visited, int x, int y, int curDir) {
         visited.add(new Cell(x, y));
         robot.clean();
