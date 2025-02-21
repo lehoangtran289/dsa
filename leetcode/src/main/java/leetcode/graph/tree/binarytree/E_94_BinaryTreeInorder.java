0995c8a9cc2b3d2e2922c140612eaf5e53a8d4d6
htl982 <lehoangtran289@gmail.com>
2025-02-21 20:35:18 +0700

    E_94_BinaryTreeInorder

--- leetcode/src/main/java/leetcode/graph/tree/binarytree/E_94_BinaryTreeInorder.java
+++ leetcode/src/main/java/leetcode/graph/tree/binarytree/E_94_BinaryTreeInorder.java
@@ -2,6 +2,7 @@ package leetcode.graph.tree.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class E_94_BinaryTreeInorder {
     public static void main(String[] args) {
@@ -19,5 +20,25 @@ public class E_94_BinaryTreeInorder {
         visit(root.left, res);
         res.add(root.val);
         visit(root.right, res);
+    }
+
+    // ---------------------------------------------------
+
+    public static List<Integer> inorderTraversalStack(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+
+        return res;
     }
 }
