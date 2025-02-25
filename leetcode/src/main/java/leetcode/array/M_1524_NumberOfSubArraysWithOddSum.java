de5c626d1ddd564039bd608f98383229fc6007d7
htl982 <lehoangtran289@gmail.com>
2025-02-26 00:16:01 +0700

    M_1524_NumberOfSubArraysWithOddSum

--- leetcode/src/main/java/leetcode/array/M_1524_NumberOfSubArraysWithOddSum.java
+++ leetcode/src/main/java/leetcode/array/M_1524_NumberOfSubArraysWithOddSum.java
@@ -7,8 +7,21 @@ public class M_1524_NumberOfSubArraysWithOddSum {
     }
 
     /**
-     * If current prefix sum is even, add the number of odd subarrays
-     * If current prefix sum is odd, add the number of even subarrays
+     * We can make a crucial observation: </br>
+     * - If two prefix sums have the same parity, their difference will be even, meaning the subarray sum is even.</br>
+     * - If two prefix sums have different parity, their difference will be odd, meaning the subarray sum is odd.</br>
+     * ==> This leads to an efficient way to count odd subarrays as we traverse the array. </br>
+     * <p>
+     * We maintain a cumulative prefixSum while keeping track of how many times we've seen an even or odd prefix sum before the current index.</br>
+     * <p>
+     * As we process each element:</br>
+     * - If prefixSum is even, it means the subarray sum from the start to the current index is even. </br>
+     *      + To form an odd subarray, we need to subtract a previously seen odd prefix sum. </br>
+     *      + So, we add the count of previously seen odd prefix sums to our answer. </br>
+     *  <p>
+     * - If prefixSum is odd, the subarray sum from the start to the current index is odd.  </br>
+     *      + To form another odd subarray, we need to subtract a previously seen even prefix sum. </br>
+     *      + So, we add the count of previously seen even prefix sums to our answer. </br>
      */
     public static int numOfSubarrays(int[] arr) {
         int modulo = (int) (Math.pow(10, 9) + 7);
