70aa79e2436c74eff8c64429a55ce49035931a3a
htl982 <lehoangtran289@gmail.com>
2025-03-31 00:41:03 +0700

    M_763_PartitionLabels

--- leetcode/src/main/java/leetcode/string/M_763_PartitionLabels.java
+++ leetcode/src/main/java/leetcode/string/M_763_PartitionLabels.java
@@ -1,16 +1,69 @@
 package leetcode.string;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class M_763_PartitionLabels {
     public static void main(String[] args) {
-        System.out.println(new M_763_PartitionLabels().partitionLabels(
-                "ababcbacadefegdehijhklij"
-        )); // [9, 7, 8]
+        System.out.println(partitionLabels1("ababcbacadefegdehijhklij")); // [9, 7, 8]
     }
 
-    public List<Integer> partitionLabels(String s) {
+    /**
+     * ---------------------------------------------------------------------------
+     * Merge intervals approach
+     * TC: O(n^2)
+     * SC: O(n)
+     * ---------------------------------------------------------------------------
+     */
+    public static List<Integer> partitionLabels1(String s) {
+        int n = s.length();
+
+        // building intervals
+        List<int[]> intervals = new ArrayList<>();
+        Set<Character> taken = new HashSet<>();
+        for (int i = 0; i < n; ++i) {
+            char c = s.charAt(i);
+            if (taken.contains(c)) continue;
+
+            for (int j = n - 1; j >= i; --j) {
+                if (s.charAt(j) == c) {
+                    intervals.add(new int[]{i, j});
+                    break;
+                }
+            }
+            taken.add(c);
+        }
+
+        // merge intervals
+        List<int[]> merged = new ArrayList<>();
+        merged.add(intervals.get(0));
+        for (int[] interval : intervals) {
+            int[] cur = merged.get(merged.size() - 1);
+            if (interval[0] <= cur[1]) {
+                cur[1] = Math.max(cur[1], interval[1]);
+            } else {
+                merged.add(interval);
+            }
+        }
+
+        // building result
+        List<Integer> res = new ArrayList<>();
+        for (int[] interval : merged) {
+            res.add(interval[1] - interval[0] + 1);
+        }
+        return res;
+    }
+
+    /**
+     * ---------------------------------------------------------------------------
+     * 2 pointers + last index array approach
+     * TC: O(n)
+     * SC: O(1)
+     * ---------------------------------------------------------------------------
+     */
+    public static List<Integer> partitionLabels(String s) {
         int n = s.length();
         int[] lastIndex = new int[26];
 
