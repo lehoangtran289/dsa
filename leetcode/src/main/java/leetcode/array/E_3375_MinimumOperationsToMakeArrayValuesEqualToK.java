b48931e2c84917c612b2cfc3f970c106fa13ecf1
lehoangtran289 <lehoangtran289@gmail.com>
2025-04-09 11:12:57 +0700

    E_3375_MinimumOperationsToMakeArrayValuesEqualToK

--- leetcode/src/main/java/leetcode/array/E_3375_MinimumOperationsToMakeArrayValuesEqualToK.java
+++ leetcode/src/main/java/leetcode/array/E_3375_MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -5,9 +5,6 @@ public class E_3375_MinimumOperationsToMakeArrayValuesEqualToK {
         System.out.println(minOperations(new int[]{5, 2, 5, 4, 5}, 2)); //2
         System.out.println(minOperations(new int[]{2, 1, 2}, 2)); //-1
         System.out.println(minOperations(new int[]{9, 7, 5, 3}, 1)); // 4
-        System.out.println(minOperations(new int[]{1}, 1)); // 0
-        System.out.println(minOperations(new int[]{2, 2}, 2)); // 0
-        System.out.println(minOperations(new int[]{6, 9, 2, 2}, 5)); // -1
     }
 
     public static int minOperations(int[] nums, int k) {
