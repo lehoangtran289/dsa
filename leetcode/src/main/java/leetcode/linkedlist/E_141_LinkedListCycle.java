22212c6d4c695c309b44c2885c44375bee3efbd6
htl982 <lehoangtran289@gmail.com>
2025-04-14 19:12:49 +0700

    E_141_LinkedListCycle

---
+++ leetcode/src/main/java/leetcode/linkedlist/E_141_LinkedListCycle.java
@@ -0,0 +1,51 @@
+package leetcode.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class E_141_LinkedListCycle {
+
+    /**
+     *  ------------------------------------------------
+     *  Intuitive solution
+     *  - Use a HashSet to store the nodes we have seen
+     *  ------------------------------------------------
+     *  TC O(n)
+     *  SC O(n)
+     */
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<>();
+        seen.add(head);
+
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            if (seen.contains(cur)) return true;
+            seen.add(cur);
+        }
+
+        return false;
+    }
+
+    /**
+     *  ------------------------------------------------
+     *  Fast and Slow pointer
+     *  - Use two pointers, one moving twice as fast as the other
+     *  ------------------------------------------------
+     *  TC O(n)
+     *  SC O(1)
+     */
+    public boolean hasCycle2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null && slow != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) return true;
+        }
+
+        return false;
+    }
+}
