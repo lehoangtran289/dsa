0365492a44dce8a379eae40afb8d34348df03cd0
htl982 <lehoangtran289@gmail.com>
2025-04-15 22:29:22 +0700

    M_437_PathSumIII

---
+++ leetcode/src/main/java/leetcode/array/prefixSum/M_437_PathSumIII.java
@@ -0,0 +1,37 @@
+package leetcode.array.prefixSum;
+
+import leetcode.graph.tree.binarytree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M_437_PathSumIII {
+    private int targetSum;
+    private int count = 0;
+    private Map<Long, Integer> freq;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        this.freq = new HashMap<>();
+        this.targetSum = targetSum;
+
+        dfs(root, 0L);
+        return count;
+    }
+
+    private void dfs(TreeNode root, long curSum) {
+        if (root == null) return;
+
+        curSum += root.val;
+        if (curSum == targetSum) count++;
+        count += freq.getOrDefault(curSum - targetSum, 0);
+
+        freq.put(curSum, freq.getOrDefault(curSum, 0) + 1);
+
+        // traverse child nodes with new curSum
+        dfs(root.left, curSum);
+        dfs(root.right, curSum);
+
+        // backtrack
+        freq.put(curSum, freq.get(curSum) - 1);
+    }
+}
