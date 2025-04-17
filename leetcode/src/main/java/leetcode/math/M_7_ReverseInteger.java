70dd9122d191ab4b695f9fc7fb8cf3baf631c35a
htl982 <lehoangtran289@gmail.com>
2025-04-17 21:17:44 +0700

    M_7_ReverseInteger

---
+++ leetcode/src/main/java/leetcode/math/M_7_ReverseInteger.java
@@ -0,0 +1,18 @@
+package leetcode.math;
+
+public class M_7_ReverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+
+        while (x != 0) {
+            int last = x % 10;
+            int temp = res * 10 + last;
+            if ((temp - last) / 10 != res) return 0;
+
+            res = temp;
+            x /= 10;
+        }
+
+        return res;
+    }
+}
