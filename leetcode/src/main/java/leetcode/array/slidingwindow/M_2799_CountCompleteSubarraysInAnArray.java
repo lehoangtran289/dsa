1613ff23ebb9f2f066709de4ca69cb670692fcea
htl982 <lehoangtran289@gmail.com>
2025-04-24 07:52:31 +0700

    M_2799_CountCompleteSubarraysInAnArray

---
+++ leetcode/src/main/java/leetcode/array/slidingwindow/M_2799_CountCompleteSubarraysInAnArray.java
@@ -0,0 +1,44 @@
+package leetcode.array.slidingwindow;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class M_2799_CountCompleteSubarraysInAnArray {
+
+    public static void main(String[] args) {
+        System.out.println(countCompleteSubarrays(new int[]{1, 3, 1, 2, 2,})); // 4
+    }
+
+    /**
+     * Sliding Window
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int countCompleteSubarrays(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums)
+            seen.add(num);
+        int uniqueCount = seen.size();
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        int n = nums.length;
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < n; ++r) {
+            freq.put(nums[r], freq.getOrDefault(nums[r], 0) + 1);
+
+            while (l <= r && freq.size() == uniqueCount) {
+                res += n - r;
+
+                freq.put(nums[l], freq.get(nums[l]) - 1);
+                if (freq.get(nums[l]) == 0) freq.remove(nums[l]);
+                l++;
+            }
+        }
+
+        return res;
+    }
+}
