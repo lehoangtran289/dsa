1243d9abcc66e86fedcea673e0ac3ca641d9cc42
htl982 <lehoangtran289@gmail.com>
2025-05-01 23:17:29 +0700

    M_424_LongestRepeatingCharacterReplacement

--- leetcode/src/main/java/leetcode/array/slidingwindow/M_424_LongestRepeatingCharacterReplacement.java
+++ leetcode/src/main/java/leetcode/array/slidingwindow/M_424_LongestRepeatingCharacterReplacement.java
@@ -1,85 +1,36 @@
 package leetcode.array.slidingwindow;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class M_424_LongestRepeatingCharacterReplacement {
     public static void main(String[] args) {
-        System.out.println(characterReplacement2("ABAB", 2)); // 4
-        System.out.println(characterReplacement2("AABABBA", 1)); // 4
-    }
-
-    // ------------APPROACH 1: Binary search O(logn) + Sliding window O(n)
-
-    public static int characterReplacement1(String s, int k) {
-        int l = 0, r = s.length();
-        int res = l;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (hasRepeatingSubstring(s, k, mid)) {
-                res = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-
-        return res;
+        System.out.println(characterReplacement("ABAB", 2)); // 4
+        System.out.println(characterReplacement("AABABBA", 1)); // 4
     }
 
     /**
-     * O(n)
-     * Sliding window with fix length
+     * Sliding Window
+     * TC: O(n)
+     * SC: O(1)
      */
-    private static boolean hasRepeatingSubstring(String s, int k, int length) {
-        Map<Character, Integer> map = new HashMap<>();
-        int maxFreq = 0;
-
-        int l = 0;
-        for (int r = 0; r < s.length(); ++r) {
-            char curChar = s.charAt(r);
-            map.put(curChar, map.getOrDefault(curChar, 0) + 1);
-
-            if (r - l + 1 > length) {
-                char startChar = s.charAt(l);
-                map.put(startChar, map.get(startChar) - 1);
-                if (map.get(startChar) == 0) map.remove(startChar);
-
-                l++;
-            }
-
-            maxFreq = Math.max(maxFreq, map.get(curChar));
-            if (length - maxFreq <= k) return true;
-        }
-        return false;
-    }
-
-    // ------------APPROACH 2: Sliding window dynamic size O(n)
-    public static int characterReplacement2(String s, int k) {
-        Map<Character, Integer> map = new HashMap<>(); // <char, freq>
-        int l = 0;
-        int maxFreq = 0;
+    public static int characterReplacement(String s, int k) {
         int res = 0;
+        int[] freq = new int[26];
+        int maxFreq = 0;
+        int l = 0;
 
         for (int r = 0; r < s.length(); ++r) {
-            char curChar = s.charAt(r);
-            map.put(curChar, map.getOrDefault(curChar, 0) + 1);
+            int rIndex = s.charAt(r) - 'A';
+            freq[rIndex]++;
 
             // the maximum frequency we have seen in any window yet
-            maxFreq = Math.max(maxFreq, map.get(curChar));
+            maxFreq = Math.max(maxFreq, freq[rIndex]);
 
-            // move the start pointer towards right if the current window is invalid
-            while (r - l + 1 - maxFreq > k) {
-                char startChar = s.charAt(l);
-                map.put(startChar, map.get(startChar) - 1);
-
-                maxFreq = Math.max(maxFreq, map.get(curChar));
-
+            while (l <= r && (r - l + 1) - maxFreq > k) {
+                int lIndex = s.charAt(l) - 'A';
+                freq[lIndex]--;
+                maxFreq = Math.max(maxFreq, freq[lIndex]);
                 l++;
             }
 
-            // the window is valid, so update the result
             res = Math.max(res, r - l + 1);
         }
 
