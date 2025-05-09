255d3508051e45b98e50c6ba308d544a74e6ec19
htl982 <lehoangtran289@gmail.com>
2025-05-09 21:13:11 +0700

    E_121_BestTimeToBuySellStock

--- leetcode/src/main/java/leetcode/array/E_121_BestTimeToBuySellStock.java
+++ leetcode/src/main/java/leetcode/array/E_121_BestTimeToBuySellStock.java
@@ -2,11 +2,10 @@ package leetcode.array;
 
 public class E_121_BestTimeToBuySellStock {
     public static void main(String[] args) {
-        E_121_BestTimeToBuySellStock obj = new E_121_BestTimeToBuySellStock();
-        System.out.println(obj.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfitKadane(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
-    public int maxProfit(int[] nums) {
+    public static int maxProfit(int[] nums) {
         int minSoFar = Integer.MAX_VALUE;
 
         int maxProfit = 0;
@@ -15,5 +14,35 @@ public class E_121_BestTimeToBuySellStock {
             maxProfit = Math.max(maxProfit, num - minSoFar); // max prof so far
         }
         return maxProfit;
+    }
+
+    /**
+     * Kadane approach
+     * Idea: We can think of the problem as finding the maximum subarray sum in the difference array
+     * -----------------------
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int maxProfitKadane(int[] prices) {
+        int n = prices.length;
+        if (n == 1) return 0;
+
+        int[] diff = new int[n];
+        for (int i = 1; i < n; ++i) {
+            diff[i] = prices[i] - prices[i - 1];
+        }
+
+        // apply find max subarray for diff
+        int res = diff[0];
+        int cur = diff[0];
+
+        for (int i = 1; i < n; ++i) {
+            if (cur < 0) cur = diff[i];
+            else cur += diff[i];
+
+            res = Math.max(res, cur);
+        }
+
+        return res;
     }
 }
