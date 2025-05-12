b40eb8e02b2280cdc7bad9882100c3890a29eb2d
htl982 <lehoangtran289@gmail.com>
2025-05-12 07:29:53 +0700

    M_122_BestTimeToBuySellStockII

--- leetcode/src/main/java/leetcode/array/heap/M_122_BestTimeToBuySellStockII.java
+++ leetcode/src/main/java/leetcode/array/heap/M_122_BestTimeToBuySellStockII.java
@@ -1,33 +1,24 @@
 package leetcode.array.heap;
 
-import java.util.PriorityQueue;
-
 public class M_122_BestTimeToBuySellStockII {
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{1, 2, 4, 2, 5, 7, 2, 4, 9, 0}));
     }
 
+    /**
+     * Peak valley approach
+     * -----------------------
+     * Try to make profit whenever the price increases
+     * TC: O(n)
+     * SC: O(1)
+     */
     public static int maxProfit(int[] prices) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        int maxProfit = 0;
 
-        int i = 1;
-        while (i < prices.length) {
-            int temp = 0;
-            while (i < prices.length && prices[i] > prices[i - 1]) {
-                temp += prices[i] - prices[i - 1];
-                ++i;
-            }
-            pq.add(temp);
-            ++i;
+        for (int i = 1; i < prices.length; ++i) {
+            if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
         }
 
-        int count = 0;
-        int res = 0;
-        while (!pq.isEmpty() && count < 2) {
-            res += pq.poll();
-            count++;
-        }
-
-        return res;
+        return maxProfit;
     }
 }
