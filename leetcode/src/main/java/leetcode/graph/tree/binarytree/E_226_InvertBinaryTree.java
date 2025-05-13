f5847ddc61ec849328dfaf9409809064f700829f
htl982 <lehoangtran289@gmail.com>
2025-05-13 22:57:44 +0700

    E_226_InvertBinaryTree

--- leetcode/src/main/java/leetcode/graph/tree/binarytree/E_226_InvertBinaryTree.java
+++ leetcode/src/main/java/leetcode/graph/tree/binarytree/E_226_InvertBinaryTree.java
@@ -1,5 +1,8 @@
 package leetcode.graph.tree.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class E_226_InvertBinaryTree {
 
     public TreeNode invertTree(TreeNode root) {
@@ -9,6 +12,25 @@ public class E_226_InvertBinaryTree {
         TreeNode left = invertTree(root.left);
         root.left = right;
         root.right = left;
+
+        return root;
+    }
+
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+
+            if (cur.left != null) queue.add(cur.left);
+            if (cur.right != null) queue.add(cur.right);
+        }
 
         return root;
     }
