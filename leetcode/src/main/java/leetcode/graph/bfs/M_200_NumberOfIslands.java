5d7b8884bd6d4faf1f4c425656af1b1aa332f34c
htl982 <lehoangtran289@gmail.com>
2025-05-31 22:08:35 +0700

    M_200_NumberOfIslands

--- leetcode/src/main/java/leetcode/graph/bfs/M_200_NumberOfIslands.java
+++ leetcode/src/main/java/leetcode/graph/bfs/M_200_NumberOfIslands.java
@@ -1,11 +1,9 @@
 package leetcode.graph.bfs;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 
 public class M_200_NumberOfIslands {
-    static int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // right, left, down, up
-
     public static void main(String[] args) {
         char[][] grid = new char[][]{
                 {'1', '1', '1', '1', '1', '1', '1', '1'},
@@ -19,37 +17,58 @@ public class M_200_NumberOfIslands {
         System.out.println(numIslands(grid));
     }
 
+    private static final int[][] DIRS = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // right, left, down, up
+
     public static int numIslands(char[][] grid) {
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    bfs(grid, i, j);
+        int rows = grid.length, cols = grid[0].length;
+        int res = 0;
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    res++;
+                    bfs(grid, visited, i, j);
                 }
             }
         }
-        return count;
+
+        return res;
     }
 
-    public static void bfs(char[][] grid, int i, int j) {
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{i, j});
+    /**
+     * bfs from (row, col) to all connected '1's
+     */
+    private static void bfs(
+            char[][] grid,
+            boolean[][] visited,
+            int row,
+            int col
+    ) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{row, col});
+        visited[row][col] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
 
-            for (int[] direction : directions) {
-                int newR = cur[0] + direction[0];
-                int newC = cur[1] + direction[1];
+            for (int[] d : DIRS) {
+                int nextX = cur[0] + d[0];
+                int nextY = cur[1] + d[1];
 
-                if (newR < 0 || newR >= grid.length || newC < 0 || newC >= grid[0].length
-                    || grid[newR][newC] != '1') // out of bounds image or not island
-                    continue;
-
-                queue.offer(new int[]{newR, newC});
-                grid[newR][newC] = '2';
+                if (
+                        isValidCell(nextX, nextY, grid.length, grid[0].length) &&
+                        grid[nextX][nextY] == '1' &&
+                        !visited[nextX][nextY]
+                ) {
+                    visited[nextX][nextY] = true;
+                    queue.add(new int[]{nextX, nextY});
+                }
             }
         }
+    }
+
+    private static boolean isValidCell(int x, int y, int rows, int cols) {
+        return x >= 0 && x < rows && y >= 0 && y < cols;
     }
 }
