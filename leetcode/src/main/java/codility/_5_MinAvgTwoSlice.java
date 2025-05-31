375afb3681b2ca53532b99a9dbac186ff6cb3613
htl982 <lehoangtran289@gmail.com>
2025-05-31 12:31:06 +0700

    _5_MinAvgTwoSlice

---
+++ leetcode/src/main/java/codility/_5_MinAvgTwoSlice.java
@@ -0,0 +1,31 @@
+package codility;
+
+public class _5_MinAvgTwoSlice {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {-3, -5, -8, -4, -10}));
+    }
+
+    public static int solution(int[] A) {
+        // Implement your solution here
+        int n = A.length;
+        int minAvgIndex = 0;
+        double minAvg = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n - 2; ++i) {
+            double curAvg2 = (double) (A[i] + A[i + 1]) / 2;
+            double curAvg3 = (double) (A[i] + A[i + 1] + A[i + 2]) / 3;
+            double curAvg = Math.min(curAvg2, curAvg3);
+
+            if (curAvg < minAvg) {
+                minAvg = curAvg;
+                minAvgIndex = i;
+            }
+        }
+
+        if ((double) (A[n - 2] + A[n - 1]) / 2 < minAvg) {
+            minAvgIndex = n - 2;
+        }
+
+        return minAvgIndex;
+    }
+}
