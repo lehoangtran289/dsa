a38186d4f2278de3c48be5c36193a6fabd20fad3
htl982 <lehoangtran289@gmail.com>
2025-05-31 16:51:12 +0700

    _4_maxCounters

---
+++ leetcode/src/main/java/codility/_4_maxCounters.java
@@ -0,0 +1,37 @@
+package codility;
+
+import java.util.Arrays;
+
+public class _4_maxCounters {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(5, new int[]{3, 4, 4, 6, 1, 4, 4}))); // [3, 2, 2, 4, 2]
+    }
+
+    public static int[] solution(int N, int[] A) {
+        // Implement your solution here
+        int n = A.length;
+        int[] res = new int[N];
+        int floor = 0;
+        int curMax = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int index = A[i] - 1;
+
+            if (1 <= A[i] && A[i] <= N) {
+                res[index] = Math.max(res[index], floor); // Ensure value is at least the floor
+                res[index]++; // do the increment
+
+                curMax = Math.max(curMax, res[index]); // maintain current max
+            } else if (A[i] == N + 1) {
+                floor = curMax; // update floor to curMax when op = N + 1
+            }
+        }
+
+        // Ensure all values are at least the floor
+        for (int i = 0; i < N; ++i) {
+            if (res[i] < floor) res[i] = floor;
+        }
+
+        return res;
+    }
+}
