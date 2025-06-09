27fbd7b93a20a08ee5d8a42af21b995699422055
htl982 <lehoangtran289@gmail.com>
2025-06-09 12:51:12 +0700

    M_49_GroupAnagrams

--- leetcode/src/main/java/leetcode/array/M_49_GroupAnagrams.java
+++ leetcode/src/main/java/leetcode/array/M_49_GroupAnagrams.java
@@ -11,6 +11,13 @@ public class M_49_GroupAnagrams {
         System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
     }
 
+    /**
+     * Given an array of strings strs, group the anagrams together.
+     * Idea; Sort + hashmap
+     * --------------------
+     * TC: O(n * k log k), where n is the number of strings and k is the maximum length of a string
+     * SC: O(n * k), where n is the number of strings and k is the maximum length of a string
+     */
     public static List<List<String>> groupAnagrams(String[] strs) {
         // group string index by sorted string
         Map<String, List<Integer>> groups = new HashMap<>();
@@ -18,18 +25,23 @@ public class M_49_GroupAnagrams {
         for (int i = 0; i < strs.length; ++i) {
             char[] charArr = strs[i].toCharArray();
             Arrays.sort(charArr);
-            String sortedStr = new String(charArr);
-            groups.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(i);
+            String key = String.valueOf(charArr);
+
+            if (!groups.containsKey(key)) {
+                groups.put(key, new ArrayList<>());
+            }
+            groups.get(key).add(i);
         }
 
         // build result
         List<List<String>> res = new ArrayList<>();
-        for (String key : groups.keySet()) {
-            List<String> anagrams = new ArrayList<>();
-            for (int id : groups.get(key)) {
-                anagrams.add(strs[id]);
+
+        for (var entry : groups.entrySet()) {
+            List<String> group = new ArrayList<>();
+            for (int index : entry.getValue()) {
+                group.add(strs[index]);
             }
-            res.add(anagrams);
+            res.add(group);
         }
 
         return res;
