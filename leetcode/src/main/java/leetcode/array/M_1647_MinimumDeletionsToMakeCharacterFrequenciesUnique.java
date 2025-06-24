aa60560d2417ecfc7f0c9be11e15618031ce0a5f
htl982 <lehoangtran289@gmail.com>
2025-06-24 22:06:07 +0700

    M_1647_MinimumDeletionsToMakeCharacterFrequenciesUnique

---
+++ leetcode/src/main/java/leetcode/array/M_1647_MinimumDeletionsToMakeCharacterFrequenciesUnique.java
@@ -0,0 +1,37 @@
+package leetcode.array;
+
+import java.util.*;
+
+public class M_1647_MinimumDeletionsToMakeCharacterFrequenciesUnique {
+    public static void main(String[] args) {
+        System.out.println(minDeletions("aaabbbcc")); // 2
+    }
+
+    /**
+     * Greedy approach
+     * Idea: Count frequency, then try decrement frequency
+     * --------------------
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public static int minDeletions(String s) {
+        // Count frequency of each character
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // process del frequency
+        int res = 0;
+        Set<Integer> seen = new HashSet<>();
+
+        for (int f : freq) {
+            while (f > 0 && seen.contains(f)) {
+                f--;
+                res++;
+            }
+            seen.add(f);
+        }
+        return res;
+    }
+}
