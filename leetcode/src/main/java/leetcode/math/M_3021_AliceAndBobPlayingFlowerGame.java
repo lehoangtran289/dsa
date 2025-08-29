50676db8e9f840ef91d4d624f554ea8873033667
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-08-29 10:24:47 -0400

    M_3021_AliceAndBobPlayingFlowerGame

---
+++ leetcode/src/main/java/leetcode/math/M_3021_AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,34 @@
+package leetcode.math;
+
+public class M_3021_AliceAndBobPlayingFlowerGame {
+    public static void main(String[] args) {
+        M_3021_AliceAndBobPlayingFlowerGame game = new M_3021_AliceAndBobPlayingFlowerGame();
+        System.out.println(game.flowerGame(2, 3)); // 3
+        System.out.println(game.flowerGame(1, 4)); // 2
+        System.out.println(game.flowerGame(3, 3)); // 4
+    }
+
+    /**
+     *  (x, y) must has different parity
+     */
+    public long flowerGame(int n, int m) {
+        long oddsInFirst = countOddInRange(1, n);
+        long evensInFirst = countEvenInRange(1, n);
+        long oddsInSecond = countOddInRange(1, m);
+        long evensInSecond = countEvenInRange(1, m);
+
+        return oddsInFirst * evensInSecond + evensInFirst * oddsInSecond;
+    }
+
+    private int countOddInRange(int x, int y) {
+        if ((x & 1) == 0) x++;
+        if ((y & 1) == 0) y--;
+        return x <= y ? (y - x) / 2 + 1 : 0;
+    }
+
+    private int countEvenInRange(int x, int y) {
+        if ((x & 1) == 1) x++;
+        if ((y & 1) == 1) y--;
+        return x <= y ? (y - x) / 2 + 1 : 0;
+    }
+}
