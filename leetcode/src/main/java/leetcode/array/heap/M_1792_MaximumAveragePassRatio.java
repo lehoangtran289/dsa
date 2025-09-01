5c0460640d238dff7316c711e9178e048942e290
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-09-01 11:34:40 -0400

    M_1792_MaximumAveragePassRatio

--- leetcode/src/main/java/leetcode/array/heap/M_1792_MaximumAveragePassRatio.java
+++ leetcode/src/main/java/leetcode/array/heap/M_1792_MaximumAveragePassRatio.java
@@ -1,6 +1,6 @@
 package leetcode.array.heap;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class M_1792_MaximumAveragePassRatio {
     public static void main(String[] args) {
@@ -9,31 +9,33 @@ public class M_1792_MaximumAveragePassRatio {
     }
 
     public static double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                (a, b) -> Double.compare(calculateGain(b[0], b[1]), calculateGain(a[0], a[1]))
-        );
+        Queue<double[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> Double.compare(b[2], a[2]));
 
-        for (int[] clazz : classes) {
-            pq.add(new int[]{clazz[0], clazz[1]});
-        }
-
-        // distributed extra students
-        while (extraStudents-- > 0 && !pq.isEmpty()) {
-            int[] clazz = pq.poll();
-            pq.add(new int[]{clazz[0] + 1, clazz[1] + 1});
-        }
-
-        // calculate final pass ratio
         double res = 0;
-        while (!pq.isEmpty()) {
-            int[] clazz = pq.poll();
-            res += (double) clazz[0] / clazz[1];
+        for (int[] c : classes) {
+            maxHeap.add(new double[]{c[0], c[1], calculateGain(c[0], c[1])});
+            res += (double) c[0] / c[1];
+        }
+
+        while (extraStudents-- > 0) {
+            double[] c = maxHeap.poll();
+
+            // update res after adding student
+            double gain = calculateGain((int) c[0], (int) c[1]);
+            res += gain;
+
+            // update new class
+            c[0]++;
+            c[1]++;
+            c[2] = calculateGain((int) c[0], (int) c[1]);
+            maxHeap.add(c);
         }
 
         return res / classes.length;
     }
 
-    private static double calculateGain(int passes, int total) {
-        return (double) (passes + 1) / (total + 1) - (double) passes / total;
+    private static double calculateGain(int pass, int total) {
+        return (double) (pass + 1) / (total + 1) - (double) pass / total;
     }
 }
