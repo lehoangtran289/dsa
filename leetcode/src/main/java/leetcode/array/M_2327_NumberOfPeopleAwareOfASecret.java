1f2e5a86910626d6c2948866c7a44240b3e54917
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-09-09 17:01:29 -0400

    M_2327_NumberOfPeopleAwareOfASecret

---
+++ leetcode/src/main/java/leetcode/array/M_2327_NumberOfPeopleAwareOfASecret.java
@@ -0,0 +1,34 @@
+package leetcode.array;
+
+public class M_2327_NumberOfPeopleAwareOfASecret {
+
+    /**
+     * Idea: maintain an array knows[n + 1], where knows[i] indicates number of ppl know at day i
+     * - knows[i] people start sharing at [i + delay, i + forget]
+     * - in the end, only people from [n - forget + 1, n] will know secret
+     */
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        final int MOD = (int) 1e9 + 7;
+
+        // knows[i] = number of people knows at day i
+        long[] knows = new long[n + 1];
+        knows[1] = 1;
+
+        // iterate for each day
+        for (int i = 1; i <= n; ++i) {
+            int l = i + delay;
+            int r = Math.min(n, i + forget - 1);
+
+            // update range of [delay, forget]
+            for (int j = l; j <= r; ++j) {
+                knows[j] = (knows[j] + knows[i]) % MOD;
+            }
+        }
+
+        long res = 0;
+        for (int i = n - forget + 1; i <= n; ++i) {
+            res = (res + knows[i]) % MOD;
+        }
+        return (int) res;
+    }
+}
