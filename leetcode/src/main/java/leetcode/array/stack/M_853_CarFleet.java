f908f9a7c49dc72aadde02cf4ac12e814e6adf91
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-10-12 11:17:40 -0400

    M_853_CarFleet

--- leetcode/src/main/java/leetcode/array/stack/M_853_CarFleet.java
+++ leetcode/src/main/java/leetcode/array/stack/M_853_CarFleet.java
@@ -1,6 +1,7 @@
 package leetcode.array.stack;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class M_853_CarFleet {
     public static void main(String[] args) {
@@ -9,45 +10,47 @@ public class M_853_CarFleet {
         System.out.println(carFleet(100, new int[]{0, 2, 4}, new int[]{4, 2, 1})); // 1
     }
 
+    static class Car {
+        int position;
+        double timeToTarget;
+
+        Car (int position, double timeToTarget) {
+            this.position = position;
+            this.timeToTarget = timeToTarget;
+        }
+    }
+
     /**
-     * Greedy, sorting
-     * Idea: Sort the cars by their position in descending order.
-     * Then, iterate through the sorted cars and check if the current car can catch up to the next car.
-     * If it can, they form a fleet
-     * ----------------------------------------------------
+     * Monotonic Stack
+     * Idea: A car can catch up the car in front of it if its time to target is less than or equal to the car in front of it
+     * --> Sort cars by position, then use a mono decreasing stack to calculate car fleet
+     * -----------------------------
      * TC: O(nlogn)
      * SC: O(n)
      */
     public static int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
+
+        // init Car array
         Car[] cars = new Car[n];
         for (int i = 0; i < n; ++i) {
             cars[i] = new Car(position[i], (double) (target - position[i]) / speed[i]);
         }
+        Arrays.sort(cars, (a, b) -> a.position - b.position);
 
-        Arrays.sort(cars, (a, b) -> a.pos - b.pos);
+        // mono decreasing stack for car fleet calculation
+        Stack<Car> carStack = new Stack<>();
 
-        int res = 0;
-        double minTime = 0;
-        for (int i = n - 1; i > 0; --i) {
-            if (cars[i].time < cars[i - 1].time) { // if the current car cannot catch up to the prev car
-                res++;
-            } else {
-                cars[i - 1].time = cars[i].time;
-                minTime = Math.min(minTime, cars[i - 1].time);
+        for (Car car : cars) {
+            while (
+                    !carStack.isEmpty()
+                    && carStack.peek().timeToTarget <= car.timeToTarget
+            ) {
+                carStack.pop();
             }
+            carStack.add(car);
         }
 
-        return res + (cars[0].time < minTime ? 0 : 1); // check first car
-    }
-
-    static class Car {
-        int pos;
-        double time;
-
-        Car(int pos, double time) {
-            this.pos = pos;
-            this.time = time;
-        }
+        return carStack.size();
     }
 }
