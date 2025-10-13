8a2474647a7e0564fd71a1abbb2e7382f7debef1
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-10-12 21:15:57 -0400

    E_2273_FindResultantArrayAfterRemovingAnagrams

---
+++ leetcode/src/main/java/leetcode/string/E_2273_FindResultantArrayAfterRemovingAnagrams.java
@@ -0,0 +1,43 @@
+package leetcode.string;
+
+import java.util.*;
+
+public class E_2273_FindResultantArrayAfterRemovingAnagrams {
+
+    /**
+     * Simulation
+     * ----------------
+     * TC: O(n * k) where n is the number of words and k is the maximum length of a word
+     * SC: O(n)
+     */
+    public List<String> removeAnagrams(String[] words) {
+        List<String> res = new ArrayList<>();
+        res.add(words[0]);
+
+        for (int i = 1; i < words.length; ++i) {
+            if (!isAnagram(words[i], words[i - 1])) {
+                res.add(words[i]);
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s1.length(); ++i) {
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
