b4d6528471ba8216c4175a090156857947fa690c
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-10-20 00:57:06 -0400

    H_656_CoinPath

--- leetcode/src/main/java/leetcode/dp/H_656_CoinPath.java
+++ leetcode/src/main/java/leetcode/dp/H_656_CoinPath.java
@@ -3,6 +3,11 @@ package leetcode.dp;
 import java.util.*;
 
 public class H_656_CoinPath {
+    public static void main(String[] args) {
+        System.out.println(cheapestJump(new int[]{1, 2, 4, -1, 2}, 2)); // [1,3,5]
+        System.out.println(cheapestJump(new int[]{1, 2, 4, -1, 2}, 1)); // []
+        System.out.println(cheapestJump(new int[]{0, 0, 0, 0, 0, 0}, 3)); // [1, 2, 3, 4, 5, 6]
+    }
 
     /**
      * Idea: DP bottom up for easier reconstruct state
@@ -11,7 +16,7 @@ public class H_656_CoinPath {
      * TC: O(n * maxJump)
      * SC: O(n)
      */
-    public List<Integer> cheapestJump(int[] coins, int maxJump) {
+    public static List<Integer> cheapestJump(int[] coins, int maxJump) {
         int n = coins.length;
         if (coins[n - 1] == -1) return new ArrayList<>();
 
@@ -43,5 +48,58 @@ public class H_656_CoinPath {
             }
         }
         return res;
+    }
+
+    // -------------------------------------------------------------
+    private int n;
+    private int maxJump;
+    private int[] coins;
+    private int[] memo;
+
+    /**
+     * Idea: DP top down with memoization
+     * dp[i] = min(coins[i] + dp[j]), j in range [i, maxJump + i]
+     * ---------------------
+     * TC: O(n * maxJump)
+     * SC: O(n)
+     */
+    public List<Integer> cheapestJump1(int[] coins, int maxJump) {
+        this.n = coins.length;
+        if (coins[n - 1] == -1) return new ArrayList<>();
+
+        this.maxJump = maxJump;
+        this.coins = coins;
+        this.memo = new int[coins.length];
+        Arrays.fill(memo, 1 << 30);
+
+        dp(0);
+
+        // no valid path
+        if (memo[n - 1] == 1 << 30) return new ArrayList<>();
+
+        // reconstruct coin path
+        int curCost = memo[0];
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) {
+            if (memo[i] == curCost) {
+                res.add(i + 1);
+                curCost -= coins[i];
+            }
+        }
+        return res;
+    }
+
+    private int dp(int i) {
+        // base cases
+        if (i >= n) return 0;
+        if (i == n - 1) return memo[i] = coins[i];
+        if (coins[i] == -1) return 1 << 30;
+        if (memo[i] != 1 << 30) return memo[i];
+
+        for (int j = i + 1; j <= Math.min(n - 1, i + maxJump); ++j) {
+            memo[i] = Math.min(memo[i], coins[i] + dp(j));
+        }
+        return memo[i];
     }
 }
