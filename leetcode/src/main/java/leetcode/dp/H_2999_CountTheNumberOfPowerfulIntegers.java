eccd5794f8b8c47cf52a4ab1bb440960d0385843
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-10-23 17:55:35 -0400

    _0_DigitDP

--- leetcode/src/main/java/leetcode/dp/H_2999_CountTheNumberOfPowerfulIntegers.java
+++ leetcode/src/main/java/leetcode/dp/H_2999_CountTheNumberOfPowerfulIntegers.java
@@ -3,16 +3,7 @@ package leetcode.dp;
 import java.util.Arrays;
 
 /**
- * Digit DP (Dynamic Programming on Digits) is a technique used to solve problems involving numbers within a range,
- * where the solution depends on the digits of the numbers. </br>
- * <a href="https://codeforces.com/blog/entry/53960">Digit DP intro</a>
- * <br/>
- * ---
- * <br/>
- * It is particularly useful for counting or finding properties of numbers that satisfy certain constraints, such as: </br>
- *      + Counting numbers with specific properties (e.g., palindromes, divisible by a number, etc.). </br>
- *      + Finding the sum of digits of numbers in a range. </br>
- *      + Counting numbers with a specific digit pattern. </br>
+ * Digit DP
  */
 public class H_2999_CountTheNumberOfPowerfulIntegers {
     public static void main(String[] args) {
