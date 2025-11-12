6d91e2d4538cf204a3ed011b8f5e700b76d0c1bb
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-11-12 12:25:26 -0500

    M_474_OnesAndZeroes

--- leetcode/src/main/java/leetcode/dp/M_474_OnesAndZeroes.java
+++ leetcode/src/main/java/leetcode/dp/M_474_OnesAndZeroes.java
@@ -8,6 +8,67 @@ import java.util.Arrays;
 public class M_474_OnesAndZeroes {
 
     /**
+     * Bottom up Knapsack DP, 2D DP array
+     * ----------------------------------------------
+     * dp[i][j] = max subsets using at most "i" ones and "j" zeros
+     * dp[i][j] = max(dp[i][j], dp[i - ones][j - zeros] + 1)
+     * ----------------------------------------------
+     * TC: O(L * m * n), L = strs.length
+     * SC: O(m * n)
+     */
+    public int findMaxForm2D(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String str : strs) {
+            int[] counts = countOnesZeros(str);
+
+            for (int i = m; i >= counts[0]; --i) {
+                for (int j = n; j >= counts[1]; --j) {
+                    dp[i][j] = Math.max(
+                            dp[i][j], // not pick
+                            dp[i - counts[0]][j - counts[1]] + 1 // pick
+                    );
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * Bottom up Knapsack DP, 3D DP array
+     * ------------------------------
+     * dp[i][j][k] = max subsets using first "i" elements, constraint with "j" ones and "k" zeros
+     * dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][j - ones[i]][k - zeros[i]])
+     * ----------------------------------------------
+     * TC: O(L * m * n), L = strs.length
+     * SC: O(L * m * n)
+     */
+    public int findMaxForm3D(String[] strs, int m, int n) {
+        int arrLength = strs.length;
+        int[][][] dp = new int[arrLength + 1][m + 1][n + 1];
+
+        for (int i = 1; i <= arrLength; ++i) {
+            int index = i - 1;
+            int[] count = countOnesZeros(strs[index]);
+
+            for (int j = 0; j <= m; ++j) {
+                for (int k = 0; k <= n; ++k) {
+                    if (count[0] <= j && count[1] <= k) {
+                        dp[i][j][k] = Math.max(
+                                dp[i - 1][j - count[0]][k - count[1]] + 1,
+                                dp[i - 1][j][k]
+                        );
+                    } else {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                    }
+                }
+            }
+        }
+
+        return dp[arrLength][m][n];
+    }
+
+    /**
      * Top Down Knapsack DP
      * ----------------------------------------------
      * TC: O(L * m * n), L = strs.length
@@ -33,7 +94,7 @@ public class M_474_OnesAndZeroes {
         if (i >= strs.length || curOnes < 0 || curZeros < 0) return 0;
         if (memo[i][curZeros][curOnes] != -1) return memo[i][curZeros][curOnes];
 
-        int[] counts = countFreq(strs[i]);
+        int[] counts = countOnesZeros(strs[i]);
 
         int pick = 0;
         if (counts[0] <= curZeros && counts[1] <= curOnes) {
@@ -44,63 +105,11 @@ public class M_474_OnesAndZeroes {
         return memo[i][curZeros][curOnes] = Math.max(pick, notPick);
     }
 
-    private int[] countFreq(String s) {
+    private int[] countOnesZeros(String s) {
         int[] res = new int[2];
         for (char c : s.toCharArray()) {
             res[c - '0']++;
         }
         return res;
-    }
-
-    /**
-     * Bottom up Knapsack 3D DP array
-     * ----------------------------------------------
-     * TC: O(L * m * n), L = strs.length
-     * SC: O(m * n)
-     */
-    public int findMaxForm2(String[] strs, int m, int n) {
-        int[][][] dp = new int[strs.length + 1][m + 1][n + 1];
-
-        for (int i = 1; i <= strs.length; ++i) {
-            int[] counts = countFreq(strs[i - 1]);
-
-            for (int curZeros = 0; curZeros <= m; ++curZeros) {
-                for (int curOnes = 0; curOnes <= n; ++curOnes) {
-                    if (counts[0] <= curZeros && counts[1] <= curOnes) {
-                        dp[i][curZeros][curOnes] = Math.max(
-                                dp[i - 1][curZeros][curOnes], // not pick
-                                dp[i - 1][curZeros - counts[0]][curOnes - counts[1]] + 1 // pick
-                        );
-                    } else {
-                        dp[i][curZeros][curOnes] = dp[i - 1][curZeros][curOnes];
-                    }
-                }
-            }
-        }
-        return dp[strs.length][m][n];
-    }
-
-    /**
-     * Bottom up Knapsack DP, 2D DP array
-     * ----------------------------------------------
-     * TC: O(L * m * n), L = strs.length
-     * SC: O(m * n)
-     */
-    public int findMaxForm3(String[] strs, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (String str : strs) {
-            int[] counts = countFreq(str);
-
-            for (int i = m; i >= counts[0]; --i) {
-                for (int j = n; j >= counts[1]; --j) {
-                    dp[i][j] = Math.max(
-                            dp[i][j], // not pick
-                            dp[i - counts[0]][j - counts[1]] + 1 // pick
-                    );
-                }
-            }
-        }
-        return dp[m][n];
     }
 }
