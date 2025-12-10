f2eea0738882602053e3e280d66e66522a6d0b50
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-12-09 22:36:58 -0500

    M_325_MaximumSizeSubarraySumEqualsK

--- leetcode/src/main/java/leetcode/array/prefixSum/M_325_MaximumSizeSubarraySumEqualsK.java
+++ leetcode/src/main/java/leetcode/array/prefixSum/M_325_MaximumSizeSubarraySumEqualsK.java
@@ -4,36 +4,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Given an integer array nums and an integer k, return the maximum length of a subarray that sums to k. If there is not one, return 0 instead.
+ * Given an integer array nums and an integer k,
+ * return the maximum length of a subarray that sums to k.
+ * If there is not one, return 0 instead.
  */
 public class M_325_MaximumSizeSubarraySumEqualsK {
 
     /**
-     * PREFIX SUM + HASHMAP
+     * Prefix Sum + HashMap
+     * ----------------------------------
+     * TC: O(n)
+     * SC: O(n)
      */
     public int maxSubArrayLen(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> seen = new HashMap<>();
         int n = nums.length;
         int curSum = 0;
         int res = 0;
 
         for (int i = 0; i < n; ++i) {
             curSum += nums[i];
-
-            // Check if all of the numbers seen so far sum to k.
             if (curSum == k) res = i + 1;
 
-            // If any subarray seen so far sums to k, then
-            // update the length of the longest_subarray.
-            if (freq.containsKey(curSum - k)) {
-                res = Math.max(res, i - freq.get(curSum - k));
+            int complement = curSum - k;
+            if (seen.containsKey(complement)) {
+                res = Math.max(res, i - seen.get(complement));
             }
 
-            // Only add the current prefix_sum index pair to the
-            // map if the prefix_sum is not already in the map.
-            if (!freq.containsKey(curSum)) {
-                freq.put(curSum, i);
-            }
+            seen.putIfAbsent(curSum, i);
         }
 
         return res;
