55ae54301dc70b84f667e834b5b9792320265ee8
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2025-12-12 23:10:36 -0500

    M_518_CoinChangeII

--- leetcode/src/main/java/leetcode/dp/M_518_CoinChangeII.java
+++ leetcode/src/main/java/leetcode/dp/M_518_CoinChangeII.java
@@ -2,15 +2,50 @@ package leetcode.dp;
 
 import java.util.Arrays;
 
+/**
+ * Unbound knapsack
+ */
 public class M_518_CoinChangeII {
-    private int[] coins;
-    private int[][] memo;
+    /**
+     * DP Bottom up 2D - unbounded knapsack
+     * Idea:
+     * dp[i][j] = number of combinations reach amount j, using first i coins
+     * dp[i][j] = not take + take
+     * = dp[i - 1][j] + dp[i][j - coins[i]]
+     * --------------------------------
+     * TC: O(n * amount)
+     * SC: O(n * amount)
+     */
+    public int change3(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            int coinValue = coins[i - 1];
+
+            for (int j = 1; j <= amount; ++j) {
+                if (j >= coinValue) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coinValue];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
 
     /**
      * Top down DP
      * state: [coins index][remain amount]
      * relation: dp(i, amount) = dp(i, amount - coins[i]) + dp(i + 1, amount);
      */
+    private int[] coins;
+    private int[][] memo;
+
     public int change(int amount, int[] coins) {
         this.coins = coins;
         this.memo = new int[coins.length][amount + 1];
