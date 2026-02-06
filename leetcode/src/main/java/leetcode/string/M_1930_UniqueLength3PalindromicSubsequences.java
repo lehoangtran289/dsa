7f3e9e7852d243280e2204bbe6a81324e4d7d226
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-02-06 08:54:46 -0500

    M_1930_UniqueLength3PalindromicSubsequences

--- leetcode/src/main/java/leetcode/string/M_1930_UniqueLength3PalindromicSubsequences.java
+++ leetcode/src/main/java/leetcode/string/M_1930_UniqueLength3PalindromicSubsequences.java
@@ -1,38 +1,61 @@
 package leetcode.string;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.Arrays;
 
+/**
+ * Counts the number of unique palindromic subsequences of length 3 in a given string.
+ */
 public class M_1930_UniqueLength3PalindromicSubsequences {
     public static void main(String[] args) {
         System.out.println(countPalindromicSubsequence("bbcbaba"));
     }
 
+    /**
+     * Simulation: choose first and last index of a char and count all unique chars in between
+     * -----------------------------
+     * TC: O(26) + O(n) + O(26 * n) -> O(n)
+     * SC: O(26 * 2) -> O(1)
+     */
     public static int countPalindromicSubsequence(String s) {
-        int count = 0;
+        int n = s.length();
+        int[][] letterIndexes = new int[26][2]; // each character, we store first and last index
 
-        Map<Character, int[]> map = new HashMap<>();
-        for (int i = 0; i < s.length(); ++i) {
-            if (!map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), new int[]{i, -1});
+        // O(26)
+        for (int i = 0; i < 26; ++i) {
+            Arrays.fill(letterIndexes[i], -1);
+        }
+
+        // O(n)
+        for (int i = 0; i < n; ++i) {
+            char c = s.charAt(i);
+
+            if (letterIndexes[c - 'a'][0] == -1) {
+                letterIndexes[c - 'a'][0] = i;
             } else {
-                int[] idx = map.get(s.charAt(i));
-                idx[1] = i;
+                letterIndexes[c - 'a'][1] = i;
             }
         }
 
-        for (Map.Entry<Character, int[]> entry : map.entrySet()) {
-            int[] idx = entry.getValue();
-            Set<Character> seen = new HashSet<>();
-            for (int i = idx[0] + 1; i < idx[1]; ++i) {
-                seen.add(s.charAt(i));
-            }
+        // O(26 * n) -> O(n)
+        int res = 0;
+        for (int i = 0; i < 26; ++i) {
+            int firstIndex = letterIndexes[i][0];
+            int lastIndex = letterIndexes[i][1];
 
-            count += seen.size();
+            if (firstIndex == -1 || lastIndex == -1) continue;
+
+            boolean[] seen = new boolean[26];
+
+            for (int j = firstIndex + 1; j < lastIndex; ++j) {
+                char c = s.charAt(j);
+
+                if (!seen[c - 'a']) {
+                    res++;
+                    seen[c - 'a'] = true;
+                }
+            }
         }
 
-        return count;
+        return res;
     }
 }
