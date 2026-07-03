f7cc688fda96c8af2b1c4740975ca8cfd5a50f7c
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-07-03 13:29:56 +0700

    refactor the code

--- leetcode/src/main/java/leetcode/graph/dijkstra/M_3286_FindASafeWalkThroughAGrid.java
+++ leetcode/src/main/java/leetcode/graph/dijkstra/M_3286_FindASafeWalkThroughAGrid.java
@@ -45,49 +45,6 @@ public class M_3286_FindASafeWalkThroughAGrid {
         return costs[rows - 1][cols - 1] < health;
     }
 
-
-    /**
-     * 0-1 BFS
-     * Idea: Use a deque to perform a 0-1 BFS, where we prioritize moving to cells (0) over cells (1).
-     * ---
-     * TC: O(n * m)
-     * SC: O(n * m)
-     */
-    public boolean findSafeWalk2(List<List<Integer>> grid, int health) {
-        int rows = grid.size(), cols = grid.get(0).size();
-        int[][] costs = new int[rows][cols];
-        Deque<int[]> deque = new ArrayDeque<>();
-
-        for (int[] row : costs) Arrays.fill(row, 1 << 30);
-        deque.addLast(new int[]{0, 0, grid.get(0).get(0)});
-        costs[0][0] = grid.get(0).get(0);
-
-        while (!deque.isEmpty()) {
-            int[] cur = deque.poll();
-
-            for (int[] dir : DIRS) {
-                int nextX = cur[0] + dir[0];
-                int nextY = cur[1] + dir[1];
-
-                if (
-                        isCellValid(nextX, nextY, rows, cols)
-                        && cur[2] + grid.get(nextX).get(nextY) < costs[nextX][nextY]
-                ) {
-                    int nextCost = cur[2] + grid.get(nextX).get(nextY);
-                    costs[nextX][nextY] = nextCost;
-
-                    // If the next cell is a 1, we add it to the BACK of deque and cost++
-                    if (grid.get(nextX).get(nextY) == 1) {
-                        deque.addLast(new int[]{nextX, nextY, nextCost});
-                    } else {
-                        deque.addFirst(new int[]{nextX, nextY, nextCost});
-                    }
-                }
-            }
-        }
-        return costs[rows - 1][cols - 1] < health;
-    }
-
     private boolean isCellValid(int x, int y, int rows, int cols) {
         return x >= 0 && x < rows && y >= 0 && y < cols;
     }
