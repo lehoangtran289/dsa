33114d631f1ee0eb89965048a0f7a9209e59b508
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-07-14 12:39:27 -0400

    E_14_LongestCommonPrefix

--- leetcode/src/main/java/leetcode/string/E_14_LongestCommonPrefix.java
+++ leetcode/src/main/java/leetcode/string/E_14_LongestCommonPrefix.java
@@ -1,13 +1,35 @@
 package leetcode.string;
 
 public class E_14_LongestCommonPrefix {
-    public static void main(String[] args) {
+    static void main() {
         System.out.println(longestCommonPrefix(new String[]{"a", "ab"}));
-        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        System.out.println(longestCommonPrefix2(new String[]{"dog", "racecar", "car"}));
+    }
+
+    /**
+     * Vertical scan
+     * ---
+     * TC: O(n * m) ~ n = strs.length, m = (str with min len)
+     * SC: O(1)
+     */
+    public static String longestCommonPrefix2(String[] strs) {
+        for (int i = 0; i < strs[0].length(); ++i) {
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; ++j) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
     }
 
     /**
      * Binary search on result
+     * ---
+     * TC: O(n * m * log(m)) ~ n = strs.length, m = (str with min len)
+     * SC: O(1)
      */
     public static String longestCommonPrefix(String[] strs) {
         int l = 0, r = Integer.MAX_VALUE;
@@ -18,7 +40,7 @@ public class E_14_LongestCommonPrefix {
         // O(log(m))
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (isCommonPrefix(strs, mid)) { // O(n)
+            if (isCommonPrefix(strs, mid)) { // O(m * n)
                 l = mid + 1;
             } else {
                 r = mid - 1;
@@ -29,7 +51,8 @@ public class E_14_LongestCommonPrefix {
     }
 
     /**
-     * O(n * m) ~ n = strs.length, m = (str with min len)
+     * TC: O(n * m) ~ n = strs.length, m = (str with min len)
+     * SC: O(1)
      */
     private static boolean isCommonPrefix(String[] strs, int len) {
         String prefix = strs[0].substring(0, len);
