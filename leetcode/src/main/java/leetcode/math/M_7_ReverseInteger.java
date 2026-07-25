01a7b31f9e060e20b4ff7c06590ff849f67e1d95
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-07-25 11:23:40 -0400

    M_7_ReverseInteger

--- leetcode/src/main/java/leetcode/math/M_7_ReverseInteger.java
+++ leetcode/src/main/java/leetcode/math/M_7_ReverseInteger.java
@@ -5,11 +5,10 @@ public class M_7_ReverseInteger {
         int res = 0;
 
         while (x != 0) {
-            int last = x % 10;
-            int temp = res * 10 + last;
-            if ((temp - last) / 10 != res) return 0;
+            if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10)
+                return 0;
 
-            res = temp;
+            res = res * 10 + x % 10;
             x /= 10;
         }
 
