05790186e84b96e56e1c31fce14cd5abb542cd34
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-07-31 16:47:00 -0400

    M_17_LetterCombinationsOfAPhoneNumber

--- leetcode/src/main/java/leetcode/backtrack/M_17_LetterCombinationsOfAPhoneNumber.java
+++ leetcode/src/main/java/leetcode/backtrack/M_17_LetterCombinationsOfAPhoneNumber.java
@@ -4,46 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class M_17_LetterCombinationsOfAPhoneNumber {
-    public static void main(String[] args) {
+    private final String[] mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private List<String> res;
+    private String digits;
+
+    static void main() {
         System.out.println(new M_17_LetterCombinationsOfAPhoneNumber().letterCombinations("23"));
         System.out.println(new M_17_LetterCombinationsOfAPhoneNumber().letterCombinations("2"));
     }
 
-    private final String[] mappings = {
-            "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
-
     public List<String> letterCombinations(String digits) {
-        if (digits.isEmpty()) return new ArrayList<>();
+        this.res = new ArrayList<>();
+        this.digits = digits;
 
-        List<String> res = new ArrayList<>();
-        backtrack(res, digits, 0, new StringBuilder());
+        backtrack(0, new StringBuilder());
 
         return res;
     }
 
-    private void backtrack(
-            List<String> res,
-            String digits,
-            int curId,
-            StringBuilder curSb
-    ) {
-        if (curId == digits.length()) {
+    private void backtrack(int pos, StringBuilder curSb) {
+        if (pos >= digits.length()) {
             res.add(curSb.toString());
             return;
         }
 
-        int mappingIdx = getIndex(digits.charAt(curId));
-        String letters = mappings[mappingIdx];
-
-        for (int i = 0; i < letters.length(); ++i) {
-            curSb.append(letters.charAt(i));
-            backtrack(res, digits, curId + 1, curSb);
-            curSb.deleteCharAt(curSb.length() - 1);
+        int curDigit = digits.charAt(pos) - '2';
+        for (char c : mappings[curDigit].toCharArray()) {
+            curSb.append(c);
+            backtrack(pos + 1, curSb);
+            curSb.setLength(curSb.length() - 1);
         }
-    }
-
-    private int getIndex(char digit) {
-        return digit - '2';
     }
 }
