164ce7d2b4775e174c28522f3bb825f760640552
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-08-01 11:22:36 -0400

    M_1584_MinCostToConnectAllPoints

--- leetcode/src/main/java/leetcode/graph/mst/M_1584_MinCostToConnectAllPoints.java
+++ leetcode/src/main/java/leetcode/graph/mst/M_1584_MinCostToConnectAllPoints.java
@@ -1,41 +1,41 @@
 package leetcode.graph.mst;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class M_1584_MinCostToConnectAllPoints {
-    public static List<Edge> kruskal(int n, List<Edge> edges) {
-        List<Edge> result = new ArrayList<>();
-        edges.sort(Comparator.comparingInt(e -> e.weight));
 
-        DisjointSet ds = new DisjointSet(n);
-
-        for (Edge e : edges) {
-            int rootSrc = ds.find(e.src);
-            int rootDest = ds.find(e.dest);
-
-            if (rootSrc != rootDest) {
-                result.add(e);
-                ds.join(rootSrc, rootDest);
-            }
-        }
-        return result;
-    }
-
+    /**
+     * Idea: build edges between all points -> Use Kruskal to find MST
+     * ---
+     * TC: O(n^2 log n), where n is the number of points
+     * SC: O(n^2)
+     */
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        List<Edge> edges = new ArrayList<>();
+        DisjointSet dsu = new DisjointSet(n);
+        List<int[]> edges = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            int[] src = points[i];
-            for (int j = i + 1; j < n; j++) {
-                int[] dest = points[j];
-                int dist = Math.abs(src[0] - dest[0]) + Math.abs(src[1] - dest[1]);
-                edges.add(new Edge(i, j, dist));
+        for (int i = 0; i < n; ++i) { // O(n^2)
+            for (int j = i + 1; j < n; ++j) {
+                int dist = getDist(points[i], points[j]);
+                edges.add(new int[]{i, j, dist});
             }
         }
-        List<Edge> mst = kruskal(n, edges);
-        return mst.stream().mapToInt(e -> e.weight).sum();
+
+        edges.sort((a, b) -> Integer.compare(a[2], b[2])); // O(n^2 log n)
+
+        int res = 0;
+        for (int[] edge : edges) {
+            if (dsu.find(edge[0]) != dsu.find(edge[1])) {
+                dsu.join(edge[0], edge[1]);
+                res += edge[2];
+            }
+        }
+        return res;
+    }
+
+    private int getDist(int[] p1, int[] p2) {
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
     }
 }
