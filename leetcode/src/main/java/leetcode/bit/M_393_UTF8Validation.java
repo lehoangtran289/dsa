2d8eb1e7f66acde28f87d15f32a019c4d7348ad0
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-08-04 11:37:09 -0400

    M_393_UTF8Validation

---
+++ leetcode/src/main/java/leetcode/bit/M_393_UTF8Validation.java
@@ -0,0 +1,54 @@
+package leetcode.bit;
+
+public class M_393_UTF8Validation {
+    static void main() {
+        System.out.println(
+                validUtf8(new int[]{197, 130, 1})
+        ); // Output: true
+
+        System.out.println(
+                validUtf8(new int[]{235, 140, 4})
+        ); // Output: false);
+    }
+
+    /**
+     * Simulation
+     * ---
+     * TC: O(n)
+     */
+    public boolean validUtf8(int[] data) {
+        for (int i = 0; i < data.length; ++i) {
+            int byteSize = getByteSize(data[i]);
+
+            // check first byte
+            if (byteSize == 1 || byteSize > 4) return false;
+
+            // check following bytes
+            while (byteSize-- > 1) {
+                i++;
+                if (i >= data.length || !isFollowByte(data[i]))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int getByteSize(int num) {
+        int res = 0;
+        for (int i = 7; i >= 0; --i) {
+            if (((num >> i) & 1) == 0) {
+                break;
+            }
+            res++;
+        }
+        return res;
+    }
+
+    private boolean isFollowByte(int num) {
+        int firstMSB = (num >> 7) & 1;
+        int secondMSB = (num >> 6) & 1;
+
+        return firstMSB != 0 && secondMSB == 0;
+    }
+}
