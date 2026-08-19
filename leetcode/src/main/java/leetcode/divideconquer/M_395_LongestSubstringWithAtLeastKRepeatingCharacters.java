456dc10e7c40c621ba7af1c3dac9f3cdfc451cc3
htl982 <56795731+lehoangtran289@users.noreply.github.com>
2026-08-19 00:28:47 -0400

    M_395_LongestSubstringWithAtLeastKRepeatingCharacters

--- leetcode/src/main/java/leetcode/divideconquer/M_395_LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ leetcode/src/main/java/leetcode/divideconquer/M_395_LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -7,37 +7,42 @@ public class M_395_LongestSubstringWithAtLeastKRepeatingCharacters {
      * Idea: Find the first invalid character (frequency < k) and split the string by this character.
      * Recursively find the longest valid substring in the left and right parts.
      * ---------------------------
+     * TC: max O(n^2)
      */
     public int longestSubstring(String s, int k) {
-        return longestSubstring(s, k, 0, s.length() - 1);
+        return helper(s, k, 0, s.length() - 1);
     }
 
-    private int longestSubstring(String s, int k, int start, int end) {
+    private int helper(String s, int k, int left, int right) {
+        if (left > right) return 0;
+
+        // build frequency array
         int[] freq = new int[26];
-        for (int i = start; i <= end; ++i) {
+        for (int i = left; i <= right; ++i) {
             freq[s.charAt(i) - 'a']++;
         }
 
-        for (int i = start; i <= end; ++i) {
-            int curFreq = freq[s.charAt(i) - 'a'];
+        // process valid substring
+        for (int i = left; i <= right; ++i) {
+            char c = s.charAt(i);
 
-            if (curFreq != 0 && curFreq < k) {
+            if (freq[c - 'a'] < k) {
                 int leftEnd = i - 1;
-                int rightStart = i;
+                int rightStart = i + 1;
 
                 // skip all invalid chars
-                while (rightStart <= end && s.charAt(rightStart) == s.charAt(i)) {
+                while (rightStart < s.length() && freq[s.charAt(rightStart) - 'a'] < k) {
                     rightStart++;
                 }
 
-                return Math.max(
-                        longestSubstring(s, k, start, leftEnd),
-                        longestSubstring(s, k, rightStart, end)
-                );
+                int leftResult = helper(s, k, left, leftEnd);
+                int rightResult = helper(s, k, rightStart, right);
+
+                return Math.max(leftResult, rightResult);
             }
         }
 
-        // no invalid chars found -> whole array is longest substring
-        return end - start + 1;
+        // If we reach here, it means the entire substring is valid
+        return right - left + 1;
     }
 }
